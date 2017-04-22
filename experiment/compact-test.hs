ca81442bac8f20b279409518b820c4b2056ab47f
Ian-Woo Kim <ianwookim@gmail.com>
2017-04-22 05:02:28 +0000

    test with Data.Sequence.Seq

--- experiment/compact-test.hs
+++ experiment/compact-test.hs
@@ -2,8 +2,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
-
-
 import           Control.Monad
 import           Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BL
@@ -11,6 +9,7 @@ import qualified Data.ByteString.Unsafe     as BU
 import           Data.Compact
 import           Data.Compact.Serialize
 import           Data.Int
+import           Data.Sequence
 import           Data.Text                        (Text)
 import           Data.Vector                      (Vector,(!))
 import qualified Data.Vector                as V
@@ -34,6 +33,9 @@ gen n
 
 testvector :: Vector Value
 testvector = V.generate (giga `div` 100) gen
+
+testseq :: Seq Value
+testseq = fromList $ Prelude.take (giga `div` 100) $ cycle [gen 0, gen 1, gen 2]
 
 main0 :: IO ()
 main0 = do 
@@ -60,6 +62,17 @@ main2 = do
     hClose h
     performMajorGC
 
+main2' :: IO ()
+main2' = do
+    c <- compact testseq
+    -- c <- compactWithSharing testvector
+    let fp = "compact.bin"
+    h <- openFile fp WriteMode
+    hPutCompact h c
+    hClose h
+    performMajorGC
+
+
 main3 :: IO ()
 main3 = do
     let fp = "compact.bin"
@@ -70,6 +83,17 @@ main3 = do
     replicateM_ 10000 $ do
       n <- randomRIO (0,giga `div` 100)
       print ((getCompact c') ! n) 
+
+main3' :: IO ()
+main3' = do
+    let fp = "compact.bin"
+    r <- unsafeReadCompact @(Seq Value) fp
+    c' <- case r of
+            Left err -> fail err
+            Right x -> return x
+    replicateM_ 10000 $ do
+      n <- randomRIO (0,giga `div` 100)
+      print ((getCompact c') !? n) 
 
 
 
@@ -102,7 +126,9 @@ main = do
     "0" -> main0
     "1" -> main1
     "2" -> main2
+    "2seq" -> main2'
     "3" -> main3
+    "3seq" -> main3'
     "4" -> main4
     "5" -> main5
     _ -> error "no such option" 
