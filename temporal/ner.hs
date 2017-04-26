5fd826660f142d89e65d5db3f35298de211db535
Ian-Woo Kim <ianwookim@gmail.com>
2017-04-26 17:56:29 +0000

    use optparse-applicative. take entity list file as an argument of the program.

--- temporal/ner.hs
+++ temporal/ner.hs
@@ -7,31 +7,33 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Loops
 import           Data.Attoparsec.Text
 import qualified Data.Attoparsec.Internal.Types as AT
-import           Data.Maybe          (isJust)
-import           Data.Text           (Text)
-import qualified Data.Text    as T
-import qualified Data.Text.IO as TIO
+import           Data.Maybe                           (isJust)
+import           Data.Monoid                          ((<>))
+import           Data.Text                            (Text)
+import qualified Data.Text                      as T
+import qualified Data.Text.IO                   as TIO
 import           Data.Tree
+import qualified Options.Applicative            as O
 import           System.Console.Haskeline
-import           System.Directory                 (getDirectoryContents)
-import           System.FilePath                  ((</>),takeBaseName,takeExtensions,takeFileName)
-import           System.Environment               (getArgs)
+import           System.Directory                     (getDirectoryContents)
+import           System.FilePath                      ((</>),takeBaseName,takeExtensions,takeFileName)
+import           System.Environment                   (getArgs)
 --
 import           SearchTree
 import           Type
 import           Util.Doc
 import           View
 
+data ProgOption = ProgOption { dir :: FilePath
+                             , entityFile :: FilePath
+                             } deriving Show
 
-{- 
-testtxt = "Some people are deeply skeptical that creating a new hybrid class of devices will \
-          \help stop the momentum of tablets from Apple and companies with devices based \
-          \on Google’s Android operating system. Marc Benioff, the chief executive of Salesforce.com \
-          \and a frequent Microsoft antagonist, said customers had already shunned new types of devices, \
-          \like Microsoft’s Surface." 
+pOptions :: O.Parser ProgOption
+pOptions = ProgOption <$> O.strOption (O.long "dir" <> O.short 'd' <> O.help "Directory")
+                      <*> O.strOption (O.long "entity" <> O.short 'e' <> O.help "Entity File")
 
-testtxt2 = "As a result, labor costs in Germany have barely budged in the past decade. The wage restraint helped German companies become more competitive on world markets, preventing the loss of manufacturing jobs to Eastern Europe or China. But it also created a bigger gap between rich and poor."
--}
+progOption :: O.ParserInfo ProgOption 
+progOption = O.info pOptions (O.fullDesc <> O.progDesc "Named Entity Recognition")
 
 getPos :: Parser Int
 getPos = AT.Parser $ \t pos more _ succ' -> succ' t pos more (AT.fromPos pos)
@@ -55,28 +57,19 @@ pTreeAdv forest = skipTill anyChar p
           e <- getPos
           return (b+1,e,x)
 
-prepareForest = do
-  txt <- TIO.readFile "F7745.all_entities"
+prepareForest fp = do
+  txt <- TIO.readFile fp
   let lst = map ((\(a,b) -> (a,T.drop 1 b)) . T.breakOn "\t") . T.lines $ txt
       nentities = map (T.unpack . snd) lst
   return (foldr addTreeItem [] nentities)
 
-{- 
-main = do
-  forest <- prepareForest
-  print $ parseOnly (many (pTreeAdv forest)) testtxt2 
--}
 
 main :: IO ()
 main = do
-  forest <- prepareForest
-  args <- getArgs
-  let dir = args !! 0
-  -- putStrLn "---------------------------"
-  -- TIO.putStrLn testtxt
-  -- putStrLn "---------------------------"
-  cnts <- getDirectoryContents dir
-  let cnts' = map (dir </>) $ filter (\p -> takeExtensions p == ".maintext") cnts
+  opt <- O.execParser progOption
+  forest <- prepareForest (entityFile opt)
+  cnts <- getDirectoryContents (dir opt)
+  let cnts' = map (dir opt </>) $ filter (\p -> takeExtensions p == ".maintext") cnts
   mapM_ (process forest) cnts'
 
 
