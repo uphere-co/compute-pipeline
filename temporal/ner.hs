0eac7e3a24ae5a5e4e7257729b3c80970a840753
Ian-Woo Kim <ianwookim@gmail.com>
2017-04-26 00:56:22 +0000

    process all files in a directory

--- temporal/ner.hs
+++ temporal/ner.hs
@@ -13,17 +13,22 @@ import qualified Data.Text    as T
 import qualified Data.Text.IO as TIO
 import           Data.Tree
 import           System.Console.Haskeline
+import           System.Directory                 (getDirectoryContents)
+import           System.FilePath                  ((</>),takeBaseName,takeExtensions,takeFileName)
+import           System.Environment               (getArgs)
 --
 import           SearchTree
 import           Type
 import           Util.Doc
 import           View
 
+{- 
 testtxt = "Some people are deeply skeptical that creating a new hybrid class of devices will \
           \help stop the momentum of tablets from Apple and companies with devices based \
           \on Google’s Android operating system. Marc Benioff, the chief executive of Salesforce.com \
           \and a frequent Microsoft antagonist, said customers had already shunned new types of devices, \
           \like Microsoft’s Surface."
+-}
 
 getPos :: Parser Int
 getPos = AT.Parser $ \t pos more _ succ' -> succ' t pos more (AT.fromPos pos)
@@ -46,23 +51,39 @@ pTreeAdv forest = skipTill anyChar p
           x <- pTree forest []
           e <- getPos
           return (b+1,e,x)
-  
-main :: IO ()
-main = do
+
+prepareForest = do
   txt <- TIO.readFile "F7745.all_entities"
   let lst = map ((\(a,b) -> (a,T.drop 1 b)) . T.breakOn "\t") . T.lines $ txt
       nentities = map (T.unpack . snd) lst
-
-  let forest = foldr addTreeItem [] nentities
-
+  return (foldr addTreeItem [] nentities)
+  
+        
+main :: IO ()
+main = do
+  forest <- prepareForest
+  args <- getArgs
+  let dir = args !! 0
   -- putStrLn "---------------------------"
   -- TIO.putStrLn testtxt
   -- putStrLn "---------------------------"
-  case parseOnly (many (pTreeAdv forest)) testtxt of
+  cnts <- getDirectoryContents dir
+  let cnts' = map (dir </>) $ filter (\p -> takeExtensions p == ".maintext") cnts
+  mapM_ (process forest) cnts'
+
+
+process :: Forest Char -> FilePath -> IO ()
+process forest fp = do
+  txt <- TIO.readFile fp  
+  case parseOnly (many (pTreeAdv forest)) txt of
     Left err -> print err
     Right parsed -> do
+      putStrLn "==========================================================="
+      putStrLn $ "file: " ++ takeFileName fp
+      putStrLn "-----------------------------------------------------------"
       let f (b,e,_) = ((),b,e)
           tagged = map f parsed
-      let ann = (AnnotText . map (\(t,m)->(t,isJust m)) . tagText tagged) testtxt
+      let ann = (AnnotText . map (\(t,m)->(t,isJust m)) . tagText tagged) txt
           xss = lineSplitAnnot 80 ann
       sequence_ (concatMap (map cutePrintAnnot) xss)
+      putStrLn "==========================================================="
