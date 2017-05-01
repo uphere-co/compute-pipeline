e9c386685d83738526734e5d95defb03bbc155ca
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-01 22:36:21 +0000

    bug fix on end of named entity word by D1017

--- temporal/Annot/NER.hs
+++ temporal/Annot/NER.hs
@@ -13,36 +13,8 @@ import           Data.Tree
 import           SearchTree
 
 
-getPos :: Parser Int
-getPos = AT.Parser $ \t pos more _ succ' -> succ' t pos more (AT.fromPos pos)
 
-skipTill :: Alternative f => f a -> f b -> f b
-skipTill p end = scan'
-  where scan' = end <|> (p *> scan')
-
-tokencloser :: Parser ()
-tokencloser = void (satisfy (`elem` (" .,!?:;()-+=\"'`/\\|" :: String))) <|> endOfInput
-
-pTree :: Forest Char -> [Char] -> Parser (String,Int)
-pTree forest acc = do
-  let lst = searchForest acc forest
-  case lst of
-    [] -> do
-      e <- getPos 
-      tokencloser
-      return (acc,e)
-    _ -> do
-      x <- satisfy (\c -> c `elem` lst)
-      pTree forest (acc++[x])
-
-pTreeAdv :: Forest Char -> Parser (Int,Int,String)
-pTreeAdv forest = skipTill anyChar p
-  where p = do
-          b <- getPos
-          (x,e) <- pTree forest []
-          return (b+1,e,x)
-
-prepareForest :: FilePath -> IO (Forest Char)
+prepareForest :: FilePath -> IO (Forest (Maybe Char))
 prepareForest fp = do
   txt <- TIO.readFile fp
   let lst = map ((\(a,b) -> (a,T.drop 1 b)) . T.breakOn "\t") . T.lines $ txt
--- temporal/sutimener.hs
+++ temporal/sutimener.hs
@@ -33,6 +33,7 @@ import qualified CoreNLP.Proto.CoreNLPProtos.Timex     as Tmx
 import qualified CoreNLP.Proto.CoreNLPProtos.Token     as TK
 import qualified CoreNLP.Proto.HCoreNLPProto.ListTimex as T
 import qualified CoreNLP.Proto.HCoreNLPProto.TimexWithOffset as T
+import           SearchTree
 -- import           Type
 import           Util.Doc (slice)
 -- import           View
@@ -60,7 +61,7 @@ data TaggedResult = TaggedResult { resultSUTime :: T.ListTimex
 
 
 processAnnotation :: J ('Class "edu.stanford.nlp.pipeline.AnnotationPipeline")
-                  -> Forest Char
+                  -> Forest (Maybe Char)
                   -> Document
                   -> IO (Either String TaggedResult)
 processAnnotation pp forest doc = runEitherT $ do
@@ -70,8 +71,6 @@ processAnnotation pp forest doc = runEitherT $ do
   TaggedResult <$> (fst <$> hoistEither (messageGet lbstr_sutime))
                <*> hoistEither (parseOnly (many (pTreeAdv forest)) (doc^.doctext))
                <*> (fst <$> hoistEither (messageGet lbstr_doc))
-
--- checkSentenceTagging ::
 
 getSentenceOffsets doc = 
   let sents = toListOf (D.sentence . traverse) doc
@@ -99,11 +98,6 @@ addSUTime sents tmxs =
 
 addNER sents tags = map (addTag tags) sents
 
-
-{-   let f t = (fromIntegral (t^.T.characterOffsetBegin), fromIntegral (t^.T.characterOffsetEnd))
-      lst = map listimex 
- -}
-
   
 showHeader fp day = do
   putStrLn "==========================================================="
@@ -119,31 +113,6 @@ showSUTime txt r = do
   annotText (fmap f tmxs) txt 
 
 showNER txt parsed = annotText (map (\(b,e,_) -> ((),b,e)) parsed) txt
-
-
-showDoc txt doc = do
-  {- let sents = toListOf (D.sentence . traverse) doc
-  putStrLn "show number of tokens per each sentence:"
-  print $ map (lengthOf (S.token . traverse)) sents 
-  putStrLn "show each token"
-  mapM_ print $ zip [1..] $ map (catMaybes . toListOf (S.token . traverse . TK.word)) sents
-  putStrLn "show each timex"
-  mapM_ print $ zip [1..] $ map (catMaybes . toListOf (S.token . traverse . TK.timexValue)) sents
-  putStrLn "show starting point"
-  mapM_ print $ zip [1..] $ map (fromJust . fromJust . firstOf (S.token . traverse . TK.beginChar)) sents
-  putStrLn "show ending point"
-  mapM_ print $ zip [1..] $ map (fromJust . fromJust . lastOf (S.token . traverse . TK.endChar)) sents
-  putStrLn "show the pair of (starting,ending)"
-   -- let  combine :: Lens' s a -> Lens' s b -> Lens' s (a,b)
-   --     combine l1 l2 = lens (\s -> (view l1 s, view l2 s)) (\s (x,y) -> set l1 x (set l2 y s))
-  mapM_ print $ zip [1..] $ flip map sents $ \s -> 
-    let b = fromJust $ fromJust $ firstOf (S.token . traverse . TK.beginChar) s
-        e = fromJust $ fromJust $ lastOf  (S.token . traverse . TK.endChar) s
-    in (b,e)
-  -- -}
-  let sentidxs = getSentenceOffsets doc
-  mapM_ print $ map (addText txt) sentidxs
-  
 
 process pgconn pp forest fp= do
   let sha256 = takeBaseName fp
@@ -161,10 +130,6 @@ process pgconn pp forest fp= do
           sentswithtmx = addSUTime sents rsutime
           sentswithner = addNER sents rner
       mapM_ print sentswithner -- sentswithtmx
-      
-      -- showDoc txt rdoc
-      -- putStrLn "-----------------------------------------------------------"
-      -- showSUTime txt rsutime
       putStrLn "-----------------------------------------------------------"
       showNER txt rner
       putStrLn "==========================================================="
@@ -183,3 +148,12 @@ main = do
     pp <- prepare pcfg
     mapM_ (process pgconn pp forest) cnts'
   PGS.close pgconn
+
+{- 
+main :: IO ()
+main = do
+  let txt = "I think Microsoft will be the most successful in history."
+  opt <- execParser progOption
+  forest <- prepareForest (entityFile opt)
+  print (parseOnly (many (pTreeAdv forest)) txt)
+-}
