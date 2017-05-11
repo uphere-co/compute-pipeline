f90e45b1c8dd9eb34c6b4f0d723dc23ebd382b39
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-11 20:55:38 +0000

    convert entire directory

--- investopedia/extract.hs
+++ investopedia/extract.hs
@@ -5,14 +5,17 @@
 module Main where
 
 import           Control.Concurrent
-import           Control.Lens        hiding (children,element)
+import           Control.Lens        hiding (children,element,(<.>))
 import           Control.Monad
+import           Data.List                  (sort)
 import           Data.Maybe                 (mapMaybe)
 import qualified Data.Text          as T
 import qualified Data.Text.IO       as TIO
-import           Data.Text.Lazy     as TL
+import qualified Data.Text.Lazy     as TL
 import qualified Data.Text.Lazy.IO  as TLIO
-import           System.Environment
+import           System.Directory           (getDirectoryContents)
+import           System.Environment         (getArgs)
+import           System.FilePath            ((</>),(<.>),takeBaseName,takeExtensions)
 import           System.IO
 import           System.Process
 import           Text.Taggy.Lens
@@ -26,13 +29,30 @@ getContent (NodeContent txt) = txt --  Prelude.concatMap (x ^.. children.travers
 getContent (NodeElement e) = T.concat (e ^.. children.traverse.content)
 
 main = do
+  dir <- (!! 0) <$> getArgs
+  dirtarget <- (!! 1) <$> getArgs
   -- txt <- TLIO.readFile "goingpublic.asp"
-  txt <- TLIO.readFile "spinoff.asp"  
-  let body = Prelude.head (txt ^.. html . allNamed (only "body"))
-      layoutbody = body ^.. allAttributed (ix "class" . hasPrefix "layout-body ")
-      xs = do
-        l <- layoutbody
-        y <- l ^.. allNamed ((only "h2") `failing` (only "p"))
-        z <- join (y ^.. children)
-        return (getContent z)
-  mapM_ TIO.putStrLn xs
+  -- txt <- TLIO.readFile "spinoff.asp"
+  contents0 <- getDirectoryContents dir
+  let contents = sort $ filter (\f -> takeExtensions f == ".asp") contents0
+  forM_ contents $ \f -> do
+    let fullpath = dir </> f
+        target = dirtarget </> takeBaseName f <.> "txt"
+    process fullpath target
+    -- mapM_ print contents
+
+process fp tfp = do
+  withFile tfp WriteMode $ \h -> do
+    putStrLn fp
+    txt <- TLIO.readFile fp
+    let body = Prelude.head (txt ^.. html . allNamed (only "body"))
+        layoutbody = body ^.. allAttributed (ix "class" . hasPrefix "layout-body ")
+        xs = do
+          l <- layoutbody
+          y <- l ^.. allNamed ((only "h2") `failing` (only "p"))
+          z <- join (y ^.. children)
+          return (getContent z)
+    let result = T.intercalate "\n" xs
+    TIO.hPutStrLn h result
+    
+  -- mapM_ TIO.putStrLn xs
