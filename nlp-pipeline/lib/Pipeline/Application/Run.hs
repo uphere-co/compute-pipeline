06944f802ce6199888c3af723f5180dc39af394b
Doh Young Mo <modohyoung@gmail.com>
2017-05-17 23:00:08 +0000

    Only run2 is remained.

--- nlp-pipeline/lib/Pipeline/Application/Run.hs
+++ nlp-pipeline/lib/Pipeline/Application/Run.hs
@@ -4,21 +4,13 @@ module Pipeline.Application.Run where
 
 import           Control.Monad                   (forM_)
 import qualified Data.ByteString.Char8  as B
-import           Data.List                       (sort)
 import qualified Data.Text.Lazy.Builder as TLB   (toLazyText)
 import qualified Data.Text.Lazy.IO      as TLIO
-import           Data.Time.Calendar              (fromGregorian,Day)
 import           Language.Java          as J
-import           Options.Applicative
-import           System.Directory                (getDirectoryContents)
 import           System.Environment              (getEnv)
-import           System.FilePath                 ((</>),takeExtensions)
 --
-import           CoreNLP.Simple.Type             (PipelineConfig(PPConfig),Document(..))
+import           CoreNLP.Simple.Type             (PipelineConfig(PPConfig))
 import           CoreNLP.Simple                  (annotate,prepare)
-import           CoreNLP.Simple.Type.Simplified
---
-import           NLP.Type.PennTreebankII
 --
 import           YAML.Builder
 --
@@ -26,23 +18,6 @@ import           Annot.NER
 import           Pipeline.Source.NewsAPI.Article
 import           Pipeline.View.YAML.YAYAML()
 import           Pipeline.Util
-import           Pipeline.Type
-
-run :: IO ()
-run = do
-  filelist <- getFileList "/data/groups/uphere/intrinio/Articles/bloomberg"
-  opt <- execParser progOption
-  -- pgconn <- PGS.connectPostgreSQL (B.pack ("dbname=" ++ dbname opt))
-  forest <- prepareForest (entityFile opt)
-  cnts <- getDirectoryContents (dir opt)
-  let cnts' = map (dir opt </>) $ sort $ filter (\p -> takeExtensions p == ".maintext") cnts
-  clspath <- getEnv "CLASSPATH"
-  J.withJVM [ B.pack ("-Djava.class.path=" ++ clspath) ] $ do
-    let pcfg = PPConfig True True True True True
-    pp <- prepare pcfg
-    mapM_ (process pp forest) filelist -- cnts'
-  -- PGS.close pgconn
-
 
 run2 :: IO ()
 run2 = do
@@ -56,7 +31,6 @@ run2 = do
       doc <- getDoc txt
       ann <- annotate pp doc
       pdoc <- getProtoDoc ann
-      -- (r1, r2) <- processDoc ann
       let psents = getProtoSents pdoc
           sents  = convertProtoSents psents pdoc
           tokens = getTokens psents
