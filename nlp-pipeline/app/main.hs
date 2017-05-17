fbd45445996dfd17f1d01df0ce8dd4f3c93b1473
Doh Young Mo <modohyoung@gmail.com>
2017-05-17 04:20:36 +0000

    Util has a run function temporarily.

--- nlp-pipeline/app/main.hs
+++ nlp-pipeline/app/main.hs
@@ -6,18 +6,5 @@ import Pipeline.Util
 
 main :: IO ()
 main = do
-  return ()
-  {-
-  opt <- execParser progOption
-  pgconn <- PGS.connectPostgreSQL (B.pack ("dbname=" ++ dbname opt))
-  forest <- prepareForest (entityFile opt)
-  cnts <- getDirectoryContents (dir opt)
-  let cnts' = map (dir opt </>) $ sort $ filter (\p -> takeExtensions p == ".maintext") cnts
-  clspath <- getEnv "CLASSPATH"
-  J.withJVM [ B.pack ("-Djava.class.path=" ++ clspath) ] $ do
-    let pcfg = PPConfig True True True True True
-    pp <- prepare pcfg
-    mapM_ (process pgconn pp forest) cnts'
-  PGS.close pgconn
-  -}
+  run
 
--- nlp-pipeline/lib/Pipeline/Util.hs
+++ nlp-pipeline/lib/Pipeline/Util.hs
@@ -170,3 +170,17 @@ process pgconn pp forest fp= do
       mapM_ formatResult . sortBy (compare `on` view (_1._1)) $ combine sentswithtmx sentswithner
       putStrLn "==========================================================="
 
+run :: IO ()
+run = do
+  opt <- execParser progOption
+  pgconn <- PGS.connectPostgreSQL (B.pack ("dbname=" ++ dbname opt))
+  forest <- prepareForest (entityFile opt)
+  cnts <- getDirectoryContents (dir opt)
+  let cnts' = map (dir opt </>) $ sort $ filter (\p -> takeExtensions p == ".maintext") cnts
+  clspath <- getEnv "CLASSPATH"
+  J.withJVM [ B.pack ("-Djava.class.path=" ++ clspath) ] $ do
+    let pcfg = PPConfig True True True True True
+    pp <- prepare pcfg
+    mapM_ (process pgconn pp forest) cnts'
+  PGS.close pgconn
+
