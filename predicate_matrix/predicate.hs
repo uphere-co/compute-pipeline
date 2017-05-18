9e290dffb4c3b03d166dc4cfa903b0202304bfeb
Doh Young Mo <modohyoung@gmail.com>
2017-05-18 21:31:18 +0000

    RecordWildCards is used.

--- predicate_matrix/predicate.hs
+++ predicate_matrix/predicate.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
@@ -37,34 +38,12 @@ data Predicate = Predicate { idLang            :: Text
                            , esoRole           :: Text
                            } deriving (Show)
 
-mkPred x = Predicate { idLang            = (x !! 0)
-                     , idPOS             = (x !! 1)
-                     , idPred            = (x !! 2)
-                     , idRole            = (x !! 3)
-                     , vnClass           = (x !! 4)
-                     , vnClassNumber     = (x !! 5)
-                     , vnSubclass        = (x !! 6)
-                     , vnSubclassNumber  = (x !! 7)
-                     , vnLema            = (x !! 8)
-                     , vnRole            = (x !! 9)
-                     , wnSense           = (x !! 10)
-                     , mcrIliOffset      = (x !! 11)
-                     , fnFrame           = (x !! 12)
-                     , fnLe              = (x !! 13)
-                     , fnFrame_Element   = (x !! 14)
-                     , pbRoleset         = (x !! 15)
-                     , pbArg             = (x !! 16)
-                     , mcrBC             = (x !! 17)
-                     , mcrDomain         = (x !! 18)
-                     , mcrSUMO           = (x !! 19)
-                     , mcrTO             = (x !! 20)
-                     , mcrLexname        = (x !! 21)
-                     , mcrBLC            = (x !! 22)
-                     , wnSensefrec       = (x !! 23)
-                     , wnSynsetRelNum    = (x !! 24)
-                     , esoClass          = (x !! 25)
-                     , esoRole           = (x !! 26)
-                     }
+mkPred x = let {[ idLang , idPOS , idPred , idRole
+                , vnClass , vnClassNumber , vnSubclass , vnSubclassNumber , vnLema , vnRole
+                , wnSense , mcrIliOffset , fnFrame , fnLe , fnFrame_Element
+                , pbRoleset , pbArg , mcrBC , mcrDomain , mcrSUMO , mcrTO , mcrLexname , mcrBLC
+                , wnSensefrec , wnSynsetRelNum , esoClass , esoRole ] = x}
+           in Predicate {..}
 
 checkData txtss = do
   let len = map length txtss
