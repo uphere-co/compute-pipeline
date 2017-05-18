3a1676ddac22d3b765752d87c36a0b3bf7a231cd
Doh Young Mo <modohyoung@gmail.com>
2017-05-18 22:54:11 +0000

    get functions are added.

--- predicate_matrix/predicate.hs
+++ predicate_matrix/predicate.hs
@@ -28,7 +28,7 @@ data PredicateMatrix = PM { idLang            :: Lang      -- id:cat , id:eng , 
                           , vnSubclassNumber  :: Text      -- vn:x.y.z-1
                           , vnLema            :: Text      -- vn:w
                           , vnRole            :: Text      -- vn:w
-                          , wnSense           :: Text      -- 
+                          , wnSense           :: Text 
                           , mcrIliOffset      :: Text
                           , fnFrame           :: Text
                           , fnLe              :: Text
@@ -58,38 +58,47 @@ getPOS x = case x of
   "id:v" -> Verb
 
 getPredicate :: Text -> Predicate
-getPredicate = T.drop 3
+getPredicate txt = last $ T.splitOn ":" txt
 
 getRole :: Text -> Role
-getRole = T.drop 3 
+getRole txt = last $ T.splitOn ":" txt
+
+getVNClass txt = last $ T.splitOn ":" txt
+getVNClassNumber txt = last $ T.splitOn ":" txt
+getVNSubclass txt = last $ T.splitOn ":" txt
+getVNSubclassNumber txt = last $ T.splitOn ":" txt
+getVNLema txt = last $ T.splitOn ":" txt
+getVNRole txt = last $ T.splitOn ":" txt
+
+getWord txt = last $ T.splitOn ":" txt
 
 mkPred x = PM { idLang            = getLang (x !! 0)
               , idPOS             = getPOS (x !! 1)
               , idPred            = getPredicate (x !! 2)
               , idRole            = getRole (x !! 3)
-              , vnClass           = (x !! 4)
-              , vnClassNumber     = (x !! 5)
-              , vnSubclass        = (x !! 6)
-              , vnSubclassNumber  = (x !! 7)
-              , vnLema            = (x !! 8)
-              , vnRole            = (x !! 9)
-              , wnSense           = (x !! 10)
-              , mcrIliOffset      = (x !! 11)
-              , fnFrame           = (x !! 12)
-              , fnLe              = (x !! 13)
-              , fnFrameElement    = (x !! 14)
-              , pbRoleset         = (x !! 15)
-              , pbArg             = (x !! 16)
-              , mcrBC             = (x !! 17)
-              , mcrDomain         = (x !! 18)
-              , mcrSUMO           = (x !! 19)
-              , mcrTO             = (x !! 20)
-              , mcrLexname        = (x !! 21)
-              , mcrBLC            = (x !! 22)
-              , wnSensefrec       = (x !! 23)
-              , wnSynsetRelNum    = (x !! 24)
-              , esoClass          = (x !! 25)
-              , esoRole           = (x !! 26)
+              , vnClass           = getWord (x !! 4)
+              , vnClassNumber     = getWord (x !! 5)
+              , vnSubclass        = getWord (x !! 6)
+              , vnSubclassNumber  = getWord (x !! 7)
+              , vnLema            = getWord (x !! 8)
+              , vnRole            = getWord (x !! 9)
+              , wnSense           = getWord (x !! 10)
+              , mcrIliOffset      = getWord (x !! 11)
+              , fnFrame           = getWord (x !! 12)
+              , fnLe              = getWord (x !! 13)
+              , fnFrameElement    = getWord (x !! 14)
+              , pbRoleset         = getWord (x !! 15)
+              , pbArg             = getWord (x !! 16)
+              , mcrBC             = getWord (x !! 17)
+              , mcrDomain         = getWord (x !! 18)
+              , mcrSUMO           = getWord (x !! 19)
+              , mcrTO             = getWord (x !! 20)
+              , mcrLexname        = getWord (x !! 21)
+              , mcrBLC            = getWord (x !! 22)
+              , wnSensefrec       = getWord (x !! 23)
+              , wnSynsetRelNum    = getWord (x !! 24)
+              , esoClass          = getWord (x !! 25)
+              , esoRole           = getWord (x !! 26)
               }
   
 take' n = S.fromList . take n . S.toList
@@ -108,36 +117,36 @@ main = do
   print $ S.fromList $ map idRole totalmat
 
 
-  -- print (take 10 enmat)
+  print (take 10 enmat)
+
   
+
   {-
-  print $ take' 10 $ S.fromList $ map idLang totalmat
-  print $ take' 10 $ S.fromList $ map idPOS totalmat
-  print $ take' 10 $ S.fromList $ map idPred totalmat
-  print $ take' 10 $ S.fromList $ map idRole totalmat
-  print $ take' 10 $ S.fromList $ map vnClass totalmat
-  print $ take' 10 $ S.fromList $ map vnClassNumber totalmat
-  print $ take' 10 $ S.fromList $ map vnSubclass totalmat
-  print $ take' 10 $ S.fromList $ map vnSubclassNumber totalmat
-  print $ take' 10 $ S.fromList $ map vnLema totalmat
-  print $ take' 10 $ S.fromList $ map vnRole totalmat
+  print $ take' 50 $ S.fromList $ map idPred totalmat
+  print $ take' 50 $ S.fromList $ map idRole totalmat
+  print $ take' 50 $ S.fromList $ map vnClass totalmat
+  print $ take' 50 $ S.fromList $ map vnClassNumber totalmat
+  print $ take' 50 $ S.fromList $ map vnSubclass totalmat
+  print $ take' 50 $ S.fromList $ map vnSubclassNumber totalmat
+  print $ take' 50 $ S.fromList $ map vnLema totalmat
+  print $ take' 50 $ S.fromList $ map vnRole totalmat
   print $ take' 50 $ S.fromList $ map wnSense totalmat
-  print $ take' 10 $ S.fromList $ map mcrIliOffset totalmat
-  print $ take' 10 $ S.fromList $ map fnFrame totalmat
-  print $ take' 10 $ S.fromList $ map fnLe totalmat
-  print $ take' 10 $ S.fromList $ map fnFrameElement totalmat
-  print $ take' 10 $ S.fromList $ map pbRoleset totalmat
-  print $ take' 10 $ S.fromList $ map pbArg totalmat
-  print $ take' 10 $ S.fromList $ map mcrBC totalmat
-  print $ take' 10 $ S.fromList $ map mcrDomain totalmat
-  print $ take' 10 $ S.fromList $ map mcrSUMO totalmat
-  print $ take' 10 $ S.fromList $ map mcrTO totalmat
-  print $ take' 10 $ S.fromList $ map mcrLexname totalmat
-  print $ take' 10 $ S.fromList $ map mcrBLC totalmat
-  print $ take' 10 $ S.fromList $ map wnSensefrec totalmat
-  print $ take' 10 $ S.fromList $ map wnSynsetRelNum totalmat
-  print $ take' 10 $ S.fromList $ map esoClass totalmat
-  print $ take' 10 $ S.fromList $ map esoRole totalmat
+  print $ take' 50 $ S.fromList $ map mcrIliOffset totalmat
+  print $ take' 50 $ S.fromList $ map fnFrame totalmat
+  print $ take' 50 $ S.fromList $ map fnLe totalmat
+  print $ take' 50 $ S.fromList $ map fnFrameElement totalmat
+  print $ take' 50 $ S.fromList $ map pbRoleset totalmat
+  print $ take' 50 $ S.fromList $ map pbArg totalmat
+  print $ take' 50 $ S.fromList $ map mcrBC totalmat
+  print $ take' 50 $ S.fromList $ map mcrDomain totalmat
+  print $ take' 50 $ S.fromList $ map mcrSUMO totalmat
+  print $ take' 50 $ S.fromList $ map mcrTO totalmat
+  print $ take' 50 $ S.fromList $ map mcrLexname totalmat
+  print $ take' 50 $ S.fromList $ map mcrBLC totalmat
+  print $ take' 50 $ S.fromList $ map wnSensefrec totalmat
+  print $ take' 50 $ S.fromList $ map wnSynsetRelNum totalmat
+  print $ take' 50 $ S.fromList $ map esoClass totalmat
+  print $ take' 50 $ S.fromList $ map esoRole totalmat
   -}
   
   {-
