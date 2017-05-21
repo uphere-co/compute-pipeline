59bebd29afdb730c6aaabdc9e051dd3bdd09095e
Doh Young Mo <modohyoung@gmail.com>
2017-05-21 16:37:07 +0000

    Util and Type are separated out.

---
+++ predicate_matrix/lib/PM/Type.hs
@@ -0,0 +1,40 @@
+module PM.Type where
+
+import Data.Text        (Text)
+
+data PredicateMatrix = PM
+  { idLang           :: Lang -- ^ Language of the predicate
+  , idPOS            :: POS  -- ^ Part-of-speech of the predicate
+  , idPred           :: Predicate -- ^ Predicate  
+  , idRole           :: Role -- ^ Role
+  , vnClass          :: Text -- ^ VerbNet class
+  , vnClassNumber    :: Text -- ^ VerbNet class number
+  , vnSubclass       :: Text -- ^ VerbNet subclass
+  , vnSubclassNumber :: Text -- ^ VerbNet subclass number
+  , vnLema           :: Text -- ^ Verb lemma
+  , vnRole           :: Text -- ^ VerbNet thematic-role
+  , wnSense          :: Text -- ^ Word sense in WordNet
+  , mcrIliOffset     :: Text -- ^ ILI number in the MCR3.0
+  , fnFrame          :: Text -- ^ Frame in FrameNet
+  , fnLe             :: Text -- ^ Corresponding lexical-entry in FrameNet
+  , fnFrameElement   :: Text -- ^ Frame-element in FrameNet
+  , pbRoleset        :: Text -- ^ Predicate in PropBank
+  , pbArg            :: Text -- ^ Predicate argument in PropBank
+  , mcrBC            :: Text -- ^ Whether the verb sense is Base Concept
+                             --   or not in the MCR3.0
+  , mcrDomain        :: Text -- ^ WordNet domain aligned to WordNet 3.0 in the MCR3.0
+  , mcrSUMO          :: Text -- ^ AdimenSUMO in the MCR3.0
+  , mcrTO            :: Text -- ^ MCR Top Ontology in the MCR3.0
+  , mcrLexname       :: Text -- ^ MCR Lexicographical file name
+  , mcrBLC           :: Text -- ^ Base Level Concept of the WordNet verb sense in the MCR3.0
+  , wnSensefrec      :: Text -- ^ Frequency of the WordNet 3.0 verb sense
+  , wnSynsetRelNum   :: Text -- ^ Number of relations of the WordNet 3.0 verb sense
+  , esoClass         :: Text -- ^ Class of the ESO ontology
+  , esoRole          :: Text -- ^ Role of the ESO ontology
+  } deriving (Show)
+
+data Lang = Cat | Eng | Eus | Spa deriving (Eq,Show)
+data POS  = Verb | Noun deriving (Eq,Show,Ord)
+
+type Predicate = Text
+type Role      = Text
---
+++ predicate_matrix/lib/PM/Util.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+
+module PM.Util where
+
+import qualified Data.Set     as S
+import Data.Text       (Text)
+import qualified Data.Text    as T
+--
+import           PM.Type
+
+
+getLang x = case x of
+  "id:cat" -> Cat
+  "id:eng" -> Eng
+  "id:eus" -> Eus
+  "id:spa" -> Spa
+
+getPOS x = case x of
+  "id:n" -> Noun
+  "id:v" -> Verb
+
+getPredicate :: Text -> Predicate
+getPredicate txt = last $ T.splitOn ":" txt
+
+getRole :: Text -> Role
+getRole txt = last $ T.splitOn ":" txt
+
+getVNClass :: Text -> Text
+getVNClass txt = last $ T.splitOn ":" txt
+
+getVNClassNumber :: Text -> Text
+getVNClassNumber txt = last $ T.splitOn ":" txt
+
+getVNSubclass :: Text -> Text
+getVNSubclass txt = last $ T.splitOn ":" txt
+
+getVnSubclassNumber :: Text -> Text
+getVnSubclassNumber txt = last $ T.splitOn ":" txt
+
+getVNLema :: Text -> Text
+getVNLema txt = last $ T.splitOn ":" txt
+
+getVNRole :: Text -> Text
+getVNRole txt = last $ T.splitOn ":" txt
+
+getWord :: Text -> Text
+getWord txt = last $ T.splitOn ":" txt
+
+mkPred x = let {[ idLang' , idPOS' , idPred , idRole
+                , vnClass , vnClassNumber , vnSubclass , vnSubclassNumber , vnLema , vnRole
+                , wnSense , mcrIliOffset , fnFrame , fnLe , fnFrameElement
+                , pbRoleset , pbArg , mcrBC , mcrDomain , mcrSUMO , mcrTO , mcrLexname , mcrBLC
+                , wnSensefrec , wnSynsetRelNum , esoClass , esoRole ] = x;
+                idLang = getLang idLang';
+                idPOS  = getPOS  idPOS';
+                }
+           in PM {..}
+
+
+
+take' n = S.fromList . take n . S.toList
+idTriple x = (idPOS x, idPred x, idRole x)
+idQuad x = (idPOS x, idPred x, idRole x, mcrIliOffset x)
+id4 x = (idPOS x, idPred x, idRole x, pbArg x)
+id5 x = (idPOS x, idPred x, idRole x, mcrIliOffset x, pbArg x)
+id6 x = (idPOS x, idPred x, idRole x, mcrIliOffset x, pbArg x, pbRoleset x)
+
