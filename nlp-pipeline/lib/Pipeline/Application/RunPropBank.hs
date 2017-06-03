e0ffe4f6df65d2ad41d53af4eda7dae7ca9e7c7b
Doh Young Mo <modohyoung@gmail.com>
2017-06-03 04:35:44 +0000

    More refactoring on process routine.

--- nlp-pipeline/lib/Pipeline/Application/RunPropBank.hs
+++ nlp-pipeline/lib/Pipeline/Application/RunPropBank.hs
@@ -9,6 +9,7 @@ import           Control.Monad                   (forM,forM_)
 import           Control.Monad.Trans.Either      (EitherT(..))
 import qualified Data.ByteString.Char8  as B
 import           Data.List
+import qualified Data.Map               as M
 import           Data.Maybe                      (isNothing)
 import           Data.Text                       (Text)
 import qualified Data.Text              as T
@@ -25,9 +26,15 @@ import           Pipeline.Run
 --
 import           PM.Type
 import           WordNet.Type
+import           WordNet.Query                   (WordNetDB)
 import           CoreNLP.Simple.Type             (PipelineConfig(PPConfig))
 import           CoreNLP.Simple                  (annotate,prepare)
 import           PropBank
+
+data DB = DB { _wordDB :: WordNetDB
+             , _propDB :: RoleSetDB
+             , _predDB :: M.Map Text [LinkNet]
+             }
 
 runPB :: IO ()
 runPB = do
@@ -37,53 +44,64 @@ runPB = do
   predmat <- loadPM "/data/groups/uphere/data/NLP/PredicateMatrix.v1.3.txt"
   worddb  <- loadDB "/data/groups/uphere/data/NLP/dict"
   propdb  <- fmap constructRoleSetDB $ constructPredicateDB <$> constructFrameDB "/data/groups/uphere/data/NLP/frames"
+
+  let db = DB worddb propdb predmat
   
   J.withJVM [ B.pack ("-Djava.class.path=" ++ clspath) ] $ do
     pp <- prepare (PPConfig True True True True True False False False)
-    forM_ (take 1 flist) $ \f -> do
-      txt <- getDescription f
-      let txt' = " In a speech from the Rose Garden, Mr. Trump said the landmark 2015 pact imposed wildly \
-      \unfair environmental standards on American businesses and workers. He vowed to stand with \
-      \the people of the United States against what he called a \"draconian\" international deal. \
-      \\"I was elected to represent the citizens of Pittsburgh, not Paris,\" the president said, \
-      \drawing support from members of his Republican Party but widespread condemnation from \
-      \political leaders, business executives and environmentalists around the globe. \
-      \Mr. Trump’s decision to abandon the agreement for environmental action signed by 195 nations \
-      \is a remarkable rebuke to heads of state, climate activists, corporate executives and members \
-      \of the president's own staff, who all failed to change his mind with an intense, last-minute \
-      \lobbying blitz. The Paris agreement was intended to bind the world community into battling \
-      \rising temperatures in concert, and the departure of the Earth’s second-largest polluter is a major blow.\" "
+    forM_ (take 1 flist) $ \f -> runProcess f db pp
+  putStrLn "Program is finished!"
+
+runProcess f db pp = do
+  let worddb  = _wordDB db
+      propdb  = _propDB db
+      predmat = _predDB db
+  txt <- getDescription f    
+  doc <- getDoc txt
+  ann <- annotate pp doc
+  pdoc <- getProtoDoc ann
+  let psents = getProtoSents pdoc
+      sents  = convertProtoSents psents pdoc
+      tokens = getTokens psents
+      ukb_input = T.unpack $ mkUkbTextInput (mkUkbInput tokens)
       
-      doc <- getDoc txt'
-      ann <- annotate pp doc
-      pdoc <- getProtoDoc ann
-      let psents = getProtoSents pdoc
-          sents  = convertProtoSents psents pdoc
-          tokens = getTokens psents
-          ukb_input = T.unpack $ mkUkbTextInput (mkUkbInput tokens)
-
-      (_,wsdlst) <- getPPR ukb_input 
-
-      result <- forM wsdlst $ \w@(wid',wpos',ili',lemma') -> do
-        let wid   = T.pack (B.unpack wid')
-            wpos  = T.pack (B.unpack wpos')
-            ili   = T.pack (B.unpack ili')
-            lemma = T.pack (B.unpack lemma')
+  (_,wsdlst) <- getPPR ukb_input 
+  
+  result <- forM wsdlst $ \w@(wid',wpos',ili',lemma') -> do
+    let wid   = T.pack (B.unpack wid')
+        wpos  = T.pack (B.unpack wpos')
+        ili   = T.pack (B.unpack ili')
+        lemma = T.pack (B.unpack lemma')
             
-        -- data LexItem = LI { _lex_word :: Text, _lex_id :: Int }
-        let Right (n,_) = decimal ili
-            concept  :: Maybe ([LexItem],Text) = getQueryConcept n (extractPOS $ wpos) worddb
+    -- data LexItem = LI { _lex_word :: Text, _lex_id :: Int }
+    let Right (n,_) = decimal ili
+        concept  :: Maybe ([LexItem],Text) = getQueryConcept n (extractPOS $ wpos) worddb
 
-        print $ getQueryPM ili predmat
+    print $ getQueryPM ili predmat
 
         
-        (senseSIDofConcept :: [(Text,Maybe Int)]) <- do
-          case concept of
-            Nothing -> return []
-            Just c  -> do
-              result <- flip mapM (fst c) $ \c' -> do
-                return $ (_lex_word c',getQuerySense (_lex_word c') (_lex_id c') worddb)
-              return result
-        return senseSIDofConcept
-      putStrLn $ show (txt,result)
-  putStrLn "Program is finished!"
+    (senseSIDofConcept :: [(Text,Maybe Int)]) <- do
+      case concept of
+        Nothing -> return []
+        Just c  -> do
+          result <- flip mapM (fst c) $ \c' -> do
+            return $ (_lex_word c',getQuerySense (_lex_word c') (_lex_id c') worddb)
+          return result
+    return senseSIDofConcept
+  putStrLn $ show (txt,result)
+
+
+
+
+txt' =
+  " In a speech from the Rose Garden, Mr. Trump said the landmark 2015 pact imposed wildly \
+  \unfair environmental standards on American businesses and workers. He vowed to stand with \
+  \the people of the United States against what he called a \"draconian\" international deal. \
+  \\"I was elected to represent the citizens of Pittsburgh, not Paris,\" the president said, \
+  \drawing support from members of his Republican Party but widespread condemnation from \
+  \political leaders, business executives and environmentalists around the globe. \
+  \Mr. Trump’s decision to abandon the agreement for environmental action signed by 195 nations \
+  \is a remarkable rebuke to heads of state, climate activists, corporate executives and members \
+  \of the president's own staff, who all failed to change his mind with an intense, last-minute \
+  \lobbying blitz. The Paris agreement was intended to bind the world community into battling \
+  \rising temperatures in concert, and the departure of the Earth’s second-largest polluter is a major blow.\" "
