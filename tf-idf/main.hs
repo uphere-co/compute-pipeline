0c80f7541b0bb10d8c70e5709fca7b7ef2402e01
Doh Young Mo <modohyoung@gmail.com>
2017-06-13 06:26:36 +0000

    count map is done.

--- tf-idf/main.hs
+++ tf-idf/main.hs
@@ -2,11 +2,20 @@
 
 module Main where
 
-import Numeric.LinearAlgebra
+import           Data.List                    (foldl')
+import qualified Data.Map              as M
+-- import qualified Data.HashMap.Strict   as HM
+import qualified Data.Text             as T
+import qualified Data.Text.IO          as TIO
+import           Numeric.LinearAlgebra
 
 testM1 = [((10000,10000),1.5),((9999,9999),0.2)]
 
 main :: IO ()
 main = do
-  print $ (mkSparse testM1)
+  txt <- TIO.readFile "test.txt"
+  print txt
+  let result = foldl' (\acc x -> M.insertWith' (+) x 1 acc) M.empty (T.words txt)
+  -- print $ (mkSparse testM1)
+  print result
   putStrLn "TF-IDF App"
