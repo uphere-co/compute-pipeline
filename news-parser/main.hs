cc601496987545718027c89304b45cf550e337fa
Doh Young Mo <modohyoung@gmail.com>
2017-06-14 06:36:24 +0000

    link and publishedTime meta-info are added.

--- news-parser/main.hs
+++ news-parser/main.hs
@@ -19,9 +19,16 @@ main = do
 
   forM_ (take 1 filelist) $ \f -> do
     txt <- TIO.readFile f
-    let ml = (TL.fromStrict txt) ^.. html . allNamed (only "meta")
-        pl = (TL.fromStrict txt) ^.. html . allNamed (only "p")
+    let ltxt = TL.fromStrict txt
+        ml = ltxt ^.. html . allNamed (only "meta")
+        ll = ltxt ^.. html . allNamed (only "link")
+        pl = ltxt ^.. html . allNamed (only "p")
+        
     print $ ml ^.. traverse . attributed (ix "property" . only "og:title") . attrs . at "content"
     print $ ml ^.. traverse . attributed (ix "property" . only "og:description") . attrs . at "content"
+    print $ ml ^.. traverse . attributed (ix "name" . only "ptime") . attrs . at "content"
+
+    print $ ll ^.. traverse . attributed (ix "rel" . only "canonical") . attrs . at "href"
+  
     print $ pl ^.. traverse . attributed (ix "class" . only "story-body-text story-content") . children . traverse . content
     print $ pl ^.. traverse . attributed (ix "itemprop" . only "articleBody") . children . traverse . content
