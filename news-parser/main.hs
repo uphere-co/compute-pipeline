9553326a3546b612c75b7b6ba315417a50d3bbc3
Doh Young Mo <modohyoung@gmail.com>
2017-06-14 06:29:50 +0000

    title and summary are extracted.

--- news-parser/main.hs
+++ news-parser/main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import           Control.Lens          ((^..),ix,only)
+import           Control.Lens          ((^..),at,ix,only)
 import           Control.Monad         (forM_)
 import           Data.Foldable         (toList)
 import           Data.List             (sort)
@@ -17,8 +17,11 @@ main = do
   filelist' <- readDirectoryWith return "/data/modori/NYT.dump"
   let filelist = sort . toList $ dirTree filelist'
 
-  forM_ filelist $ \f -> do
+  forM_ (take 1 filelist) $ \f -> do
     txt <- TIO.readFile f
-    let pl = (TL.fromStrict txt) ^.. html . allNamed (only "p") 
+    let ml = (TL.fromStrict txt) ^.. html . allNamed (only "meta")
+        pl = (TL.fromStrict txt) ^.. html . allNamed (only "p")
+    print $ ml ^.. traverse . attributed (ix "property" . only "og:title") . attrs . at "content"
+    print $ ml ^.. traverse . attributed (ix "property" . only "og:description") . attrs . at "content"
     print $ pl ^.. traverse . attributed (ix "class" . only "story-body-text story-content") . children . traverse . content
     print $ pl ^.. traverse . attributed (ix "itemprop" . only "articleBody") . children . traverse . content
