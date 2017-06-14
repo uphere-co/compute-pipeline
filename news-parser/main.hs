690b7d9df6fa4d623cc029469aedf1c457689f36
Doh Young Mo <modohyoung@gmail.com>
2017-06-14 06:52:14 +0000

    Separated out the parsing part. ArticleNYT data type is also made.

--- news-parser/main.hs
+++ news-parser/main.hs
@@ -2,15 +2,48 @@
 
 module Main where
 
-import           Control.Lens          ((^..),at,ix,only)
-import           Control.Monad         (forM_)
-import           Data.Foldable         (toList)
-import           Data.List             (sort)
+import           Control.Monad.IO.Class (liftIO)
+import           Control.Lens           ((^..),at,ix,only)
+import           Control.Monad          (forM_)
+import           Data.Foldable          (toList)
+import           Data.List              (sort)
+import           Data.Text              (Text)
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.IO   as TIO
-import           System.Directory.Tree (dirTree, readDirectoryWith)
+import           System.Directory.Tree  (dirTree, readDirectoryWith)
 import           Text.Taggy.Lens
 
+data ArticleNYT = ArticleNYT
+  { _title :: [Maybe Text]
+  , _link :: [Maybe Text]
+  , _ptime :: [Maybe Text]
+  , _summary :: [Maybe Text]
+  , _maintext :: [Text]
+  } deriving (Show)
+
+parseNYT :: Text -> IO (ArticleNYT)
+parseNYT htxt = do
+  let ltxt = TL.fromStrict htxt
+      ml = ltxt ^.. html . allNamed (only "meta")
+      ll = ltxt ^.. html . allNamed (only "link")
+      pl = ltxt ^.. html . allNamed (only "p")
+             
+  let title   = ml ^.. traverse . attributed (ix "property" . only "og:title") . attrs . at "content"
+      summary = ml ^.. traverse . attributed (ix "property" . only "og:description") . attrs . at "content"
+      ptime   = ml ^.. traverse . attributed (ix "name" . only "ptime") . attrs . at "content"
+      link    = ll ^.. traverse . attributed (ix "rel" . only "canonical") . attrs . at "href"
+      maintext1 = pl ^.. traverse . attributed (ix "class" . only "story-body-text story-content") . children . traverse . content
+      maintext2 = pl ^.. traverse . attributed (ix "itemprop" . only "articleBody") . children . traverse . content
+
+  let result = ArticleNYT
+        { _title = title
+        , _link  = link
+        , _ptime = ptime
+        , _summary = summary
+        , _maintext = (maintext1 ++ maintext2)
+        }
+        
+  return result
 
 main :: IO ()
 main = do
@@ -19,16 +52,5 @@ main = do
 
   forM_ (take 1 filelist) $ \f -> do
     txt <- TIO.readFile f
-    let ltxt = TL.fromStrict txt
-        ml = ltxt ^.. html . allNamed (only "meta")
-        ll = ltxt ^.. html . allNamed (only "link")
-        pl = ltxt ^.. html . allNamed (only "p")
-        
-    print $ ml ^.. traverse . attributed (ix "property" . only "og:title") . attrs . at "content"
-    print $ ml ^.. traverse . attributed (ix "property" . only "og:description") . attrs . at "content"
-    print $ ml ^.. traverse . attributed (ix "name" . only "ptime") . attrs . at "content"
-
-    print $ ll ^.. traverse . attributed (ix "rel" . only "canonical") . attrs . at "href"
-  
-    print $ pl ^.. traverse . attributed (ix "class" . only "story-body-text story-content") . children . traverse . content
-    print $ pl ^.. traverse . attributed (ix "itemprop" . only "articleBody") . children . traverse . content
+    parsedNYT <- parseNYT txt
+    print parsedNYT
