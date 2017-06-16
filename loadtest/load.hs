b7ccb04993fbff3f919b2e4ef90133ff9004bc2d
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-16 00:40:27 +0000

    code cleanup. and leave multiple hashmap as it is.

--- loadtest/load.hs
+++ loadtest/load.hs
@@ -28,13 +28,12 @@ main = do
       neach = (n `div` 20) + 1
       txtss = chunksOf neach txts
 
-  ref <- atomically $ newTVar [] -- (zip [0..] (replicate 20 Nothing))
-  
-  flip mapM_ (zip [0..] txtss) $ \(n,txts) -> -- forkIO $ do
-    deepseq txts $ forkIO $ do
+  ref <- atomically $ newTVar []
+  flip mapM_ (zip [0..] txtss) $ \(n,txts) ->
+    forkIO $ do
       let xs = HM.fromList . map (\(x:xs) -> (x,T.intercalate " " xs)) . map T.words $ txts
       deepseq xs $ do
-        (print . HM.size) xs
+        -- (print . HM.size) xs
         atomically $ do
           array <- readTVar ref
           writeTVar ref ((n,xs):array)
@@ -42,28 +41,8 @@ main = do
   xss <- atomically $ do
            array <- readTVar ref
            if (length array /= 20) then retry else return (map snd array)
-          
-  let m = HM.unions xss
-  print (HM.lookup "Q349" m) -- (HM.size m)
-  return ()
-  -- mapM_ ( ms
-  -- print (HM.size m)
+  {- let m = HM.unions xss
+  print (HM.lookup "Q349" m)
+  -}
+  mapM_ (print . HM.lookup "Q349") xss
 
-  -- print ()
-
-
-  
-{-   withFile fp ReadMode $ \h -> do
-     let sz = hFileSize h
-        szeach = sz `div` 20
-        ptrs = map (*szeach) [0..19]
-   -}     
-    
-        
-    {- 
-    txt <- TIO.readFile fp
-    let txts = T.lines txt
-        m = HM.fromList . map (\(x:xs)-> (x,T.intercalate " " xs)) . map T.words $ txts
-        -- print (HM.size m)
-    print (HM.lookup "Q354" m)
-    -}
