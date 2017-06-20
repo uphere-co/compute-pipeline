fdf7a789ec9efc0964fdbd92e7665cdb01355b4c
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-20 23:44:06 +0000

    order by publishedAt

--- extract_description/extract.hs
+++ extract_description/extract.hs
@@ -5,7 +5,9 @@ module Main where
 import           Control.Lens
 import           Data.Aeson
 import qualified Data.ByteString.Char8 as B
-import           Data.List                    (sort)
+import           Data.Function                (on)
+import           Data.List                    (sort,sortBy)
+import           Data.Maybe                   (catMaybes)
 import           Data.Monoid                  ((<>))
 import qualified Data.Text.IO          as TIO
 import           System.Directory
@@ -13,24 +15,23 @@ import           System.FilePath
 --
 import           NewsAPI.Type (SourceArticles(..))
 
+printFormat (time,title,desc) = do
+  TIO.putStrLn time
+  TIO.putStrLn title
+  TIO.putStrLn desc
+  TIO.putStrLn ""
+                
 
 main :: IO ()
 main = do
-  let dir = "/data/groups/uphere/intrinio/Articles/bloomberg"
+  let dir = "/data/groups/uphere/repo/fetchfin/newsapi/Articles/bloomberg"
   cnts <- getDirectoryContents dir
-  let cnts' = (sort . filter (\x -> x /= "." && x /= "..")) cnts
-  flip mapM_  cnts' $ \fp -> do
+  let cnts' = (filter (\x -> x /= "." && x /= "..")) cnts
+  lst <- flip mapM cnts' $ \fp -> do
     bstr <- B.readFile (dir </> fp)
     let esrc = eitherDecodeStrict bstr :: Either String SourceArticles
     case esrc of
-      Left err -> return ()
-      Right src -> do
-        mapM_ (TIO.putStrLn . (<> ".")) (_title src)
-        mapM_ TIO.putStrLn (_description src)
-        TIO.putStrLn ""
-        
-        -- print (._title src,_description src)
-    
-  {- 
-  txt <- TIO.readFile ""
-  -}
+      Left err -> return Nothing
+      Right src -> return ((,,) <$> _publishedAt src <*> _title src <*> _description src)
+
+  mapM_ printFormat $ sortBy (compare `on` (^._1)) $ catMaybes lst 
