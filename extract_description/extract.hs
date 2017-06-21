24a3e18c92ae3a781776a9571538c21550abfccf
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-21 00:24:40 +0000

    extract verbs

--- extract_description/extract.hs
+++ extract_description/extract.hs
@@ -5,14 +5,27 @@ module Main where
 import           Control.Lens
 import           Data.Aeson
 import qualified Data.ByteString.Char8 as B
+import           Data.Default
 import           Data.Function                (on)
 import           Data.List                    (sort,sortBy)
-import           Data.Maybe                   (catMaybes)
+import           Data.Maybe                   (catMaybes,mapMaybe)
 import           Data.Monoid                  ((<>))
+import           Data.Text                    (Text)
 import qualified Data.Text.IO          as TIO
+import           Language.Java           as J
 import           System.Directory
+import           System.Environment
 import           System.FilePath
+import           Text.Printf
 --
+import qualified CoreNLP.Proto.CoreNLPProtos.Document  as D
+import qualified CoreNLP.Proto.CoreNLPProtos.Sentence  as S
+import qualified CoreNLP.Proto.CoreNLPProtos.Token     as TK
+import           CoreNLP.Simple
+import           CoreNLP.Simple.Type
+import           CoreNLP.Simple.Type.Simplified
+import           CoreNLP.Simple.Util
+import           NLP.Type.PennTreebankII
 import           NewsAPI.Type (SourceArticles(..))
 
 printFormat (time,title,desc) = do
@@ -20,18 +33,61 @@ printFormat (time,title,desc) = do
   TIO.putStrLn title
   TIO.putStrLn desc
   TIO.putStrLn ""
-                
 
+
+getTimeTitleDesc :: FilePath -> IO (Maybe (Text,Text,Text))
+getTimeTitleDesc fp = do
+  bstr <- B.readFile fp 
+  let esrc = eitherDecodeStrict bstr :: Either String SourceArticles
+  case esrc of
+    Left err -> return Nothing
+    Right src -> return ((,,) <$> _publishedAt src <*> _title src <*> _description src)
+
+
+
+
+convertToken' :: TK.Token -> Maybe Token
+convertToken' t = do
+  (b',e') <- (,) <$> t^.TK.beginChar <*> t^.TK.endChar
+  let (b,e) = (fromIntegral b',fromIntegral e')
+  w <- cutf8' <$> (t^.TK.originalText)
+  p <- identifyPOS . cutf8' <$> (t^.TK.pos)
+  l <- cutf8' <$> (t^.TK.lemma)
+  return (Token (b,e) w p l)
+
+
+formatLemmaPOS t = printf "%10s %5s" (t^.token_lemma) (show (t^.token_pos))
+  
 main :: IO ()
 main = do
+  
   let dir = "/data/groups/uphere/repo/fetchfin/newsapi/Articles/bloomberg"
   cnts <- getDirectoryContents dir
-  let cnts' = (filter (\x -> x /= "." && x /= "..")) cnts
-  lst <- flip mapM cnts' $ \fp -> do
-    bstr <- B.readFile (dir </> fp)
-    let esrc = eitherDecodeStrict bstr :: Either String SourceArticles
-    case esrc of
-      Left err -> return Nothing
-      Right src -> return ((,,) <$> _publishedAt src <*> _title src <*> _description src)
+  let cnts' = take 10 $ (filter (\x -> x /= "." && x /= "..")) cnts
+  lst <- flip mapM cnts' $ \fp -> getTimeTitleDesc (dir </> fp)
+  let ordered = sortBy (compare `on` (^._1)) $ catMaybes lst 
 
-  mapM_ printFormat $ sortBy (compare `on` (^._1)) $ catMaybes lst 
+
+  clspath <- getEnv "CLASSPATH"
+  J.withJVM [ B.pack ("-Djava.class.path=" ++ clspath) ] $ do
+    pp <- prepare (def & (tokenizer .~ True)
+                       . (words2sentences .~ True)
+                       . (postagger .~ True)
+                       . (lemma .~ True)
+                  )
+    let (_,title,desc) = head ordered
+    doc <- getDoc desc
+    ann <- annotate pp doc
+    pdoc <- getProtoDoc ann
+    print doc
+    print pdoc
+    let psents = getProtoSents pdoc
+        sents = map (convertSentence pdoc) psents
+        tktokss = map (getTKTokens) psents
+        tokss = map (mapMaybe convertToken') tktokss
+    --     tokens = getAllTokens psents
+    -- mapM_ print sents -- tokens
+    -- mapM_ print tktokss
+    mapM_ (mapM_ (putStrLn.formatLemmaPOS)
+                 . filter (\t -> isVerb (t^.token_pos))) tokss
+  -- mapM_ printFormat $ sortBy (compare `on` (^._1)) $ catMaybes lst 
--- extract_description/shell.nix
+++ extract_description/shell.nix
@@ -1,25 +1,52 @@
 { pkgs ? import <nixpkgs> {}
 , uphere-nix-overlay ? <uphere-nix-overlay>
 , fetchfin           ? <fetchfin>
+, HCoreNLP           ? <HCoreNLP>
+, HUKB               ? <HUKB>
+, HWordNet           ? <HWordNet>
+, nlp-types          ? <nlp-types>
+, predicate-matrix   ? <predicate-matrix>
+, PropBank           ? <PropBank>
+, wiki-ner           ? <wiki-ner>
+, textview           ? <textview>
 }:
 
 with pkgs;
 
 let
-  hsconfig = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.0.x.nix")
-               { inherit pkgs; };
+  res_corenlp = import (uphere-nix-overlay + "/nix/linguistic-resources/corenlp.nix") {
+    inherit fetchurl fetchzip srcOnly;
+  };
+  corenlp = res_corenlp.corenlp;
+  corenlp_models = res_corenlp.corenlp_models;
 
+  config1 = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.0.x.nix")
+               { inherit pkgs; };
   config2 =
     self: super: {
+      "nlp-types" = self.callPackage (import nlp-types) {};
+      "textview" = self.callPackage (import textview) {};
       "newsapi" = self.callPackage (import (fetchfin + "/newsapi")) {};
-    };
-
-  newHaskellPackages = haskellPackages.override {
-    overrides = self: super: hsconfig self super // config2 self super;
+      "HCoreNLP" = self.callPackage (import HCoreNLP) { inherit jdk corenlp corenlp_models; };
+      "HCoreNLP-Proto" = self.callPackage (import (HCoreNLP + "/HCoreNLP-Proto")) {};       
+      "HWordNet" = self.callPackage (import HWordNet) {};
+      "predicate-matrix" = self.callPackage (import predicate-matrix) {};
+      "PropBank" = self.callPackage (import PropBank) {};
+      "wiki-ner" = self.callPackage (import wiki-ner) {};
   };
+  ukb = import (uphere-nix-overlay + "/nix/cpp-modules/ukb.nix") { inherit stdenv fetchgit fetchurl boost; };
+  config3 = import (HUKB + "/HUKB-driver/config.nix") { inherit pkgs uphere-nix-overlay ukb; };
+  config4 =
+    self: super: {
+      "HUKB-driver" = self.callPackage (import (HUKB + "/HUKB-driver")) {}; 
+    };  
+  myhaskellpkgs = haskell.packages.ghc802.override {
+    overrides = self: super: config1 self super // config2 self super // config3 self super // config4 self super;
+  }; 
 
 
-  hsenv = newHaskellPackages.ghcWithPackages (p: with p; [
+
+  hsenv = myhaskellpkgs.ghcWithPackages (p: with p; [
             aeson
             attoparsec
             bifunctors
@@ -27,6 +54,7 @@ let
             text
             cabal-install
             p.newsapi
+            p.HCoreNLP
           ]);
 
 in
@@ -35,6 +63,7 @@ stdenv.mkDerivation {
   name = "extract-dev";
   buildInputs = [ hsenv ];
   shellHook = ''
-  '';
+    CLASSPATH="${corenlp_models}:${corenlp}/stanford-corenlp-3.7.0.jar:${corenlp}/protobuf.jar:${corenlp}/joda-time.jar:${corenlp}/jollyday.jar:${hsenv}/share/x86_64-linux-ghc-8.0.2/HCoreNLP-0.1.0.0/HCoreNLPProto.jar";
+'';
 }
 
