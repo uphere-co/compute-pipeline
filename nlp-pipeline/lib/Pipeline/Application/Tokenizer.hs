8a9619dbf53355eacf7558f0a7848192629f704e
Doh Young Mo <modohyoung@gmail.com>
2017-07-07 02:50:15 +0000

    By using redis, save on-processing articles.

--- nlp-pipeline/lib/Pipeline/Application/Tokenizer.hs
+++ nlp-pipeline/lib/Pipeline/Application/Tokenizer.hs
@@ -3,18 +3,24 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Pipeline.Application.Tokenizer where
-
-import           Data.Maybe                      (fromMaybe,mapMaybe)
+import           Control.Monad.Trans.Class       (lift)
+import           Control.Monad.IO.Class          (liftIO)
+import           Data.Maybe                      (catMaybes,fromMaybe,mapMaybe)
 import           Data.Binary                     (Binary,encode)
-import           Control.Monad                   (forM_)
+import           Control.Monad                   (forM_,when)
+import           Data.ByteString                 (ByteString)
 import qualified Data.ByteString.Char8  as B
 import qualified Data.ByteString.Lazy   as BL
 import           Data.Text                       (Text)
 import qualified Data.Text              as T
+import           Database.Redis
 import           Language.Java          as J
 import           System.Environment              (getEnv)
 import           GHC.Generics
 import qualified NYT.Type               as NYT
+
+import qualified Control.Monad.State    as State
+import           Control.Monad.Loops
 --
 import           CoreNLP.Simple                  (annotate,prepare)
 import           CoreNLP.Simple.Type             (PipelineConfig(PPConfig))
@@ -35,11 +41,42 @@ instance Binary TokenizedNYTArticle
 
 runTokenizer :: Int -> IO ()
 runTokenizer n = do
-  particles' <- getAllParsedNYTArticle
+  particles'' <- getAllParsedNYTArticle
   sanalyses <- getAllAnalyzedNYTArticle
 
-  let particles = take n $ filter (\(h,f) -> not (h `elem` sanalyses)) particles' 
-  
+  let particles' = filter (\(h,f) -> not (h `elem` sanalyses)) particles''
+
+  conn <- checkedConnect defaultConnectInfo { connectHost = "localhost", connectPort = PortNumber 11111 }
+  result <- runRedis conn $ do
+    a' <- fmap catMaybes $ do
+      _ <- flip State.evalStateT 0 $ do
+        flip takeWhileM particles' $ \(hsh,article) -> do
+          m <- State.get
+          eb <- lift $ exists (B.pack hsh)
+          let Right b = eb
+          if b
+            then return True
+            else do            
+            lift $ setnx (B.pack hsh) ("tokenization" :: ByteString)
+            liftIO $ print hsh
+            if (m+1 >= n) then return False else (State.put (m+1) >> return True)                        
+      return [Nothing]
+    return Nothing
+
+  print ""
+
+          {-
+      eb <- exists hsh
+      case eb of
+        Left  reply -> error "error!"
+        Right b     -> if b then return Nothing else return (Just x)
+    quit
+    return a'
+  print result
+
+
+  -}
+  {-
   clspath <- getEnv "CLASSPATH"
   J.withJVM [ B.pack ("-Djava.class.path=" ++ clspath) ] $ do
     pp <- prepare (PPConfig True True True True False False False True)
@@ -58,8 +95,9 @@ runTokenizer n = do
             , _tokenizedMaintext = mtk }
 
       let savepath = "/data/groups/uphere/news-archive/fetchfin/nyt/NYTArticles/" ++  hsh ++ ".info/" ++ hsh ++ ".tokenized"
-      BL.writeFile savepath (encode tokenizednyt)
-      
+      return ()
+      -- BL.writeFile savepath (encode tokenizednyt)
+   -}
 getSimplifiedTokensFromText txt pp = do
   doc <- getDoc txt
   ann <- annotate pp doc
