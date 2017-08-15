f108152d100e26eaa78e17010072f14e3a9c9aad
Doh Young Mo <modohyoung@gmail.com>
2017-08-15 05:39:23 +0000

    Tried concurrency library, but fails due to JVM.

--- app/corenlp-runner.hs
+++ app/corenlp-runner.hs
@@ -2,15 +2,18 @@
 
 module Main where
 
+import           Control.Concurrent
 import           Control.Lens
-import           Control.Monad              (forM_)
+import           Control.Monad              (forM_,replicateM_,void)
 import qualified Data.Aeson                 as A
 import qualified Data.ByteString.Char8      as B
 import qualified Data.ByteString.Lazy.Char8 as BL
 import           Data.Default
+import           Data.List.Split            (chunksOf)
 import           Data.Maybe                 (catMaybes)
 import qualified Data.Text                  as T
 import qualified Data.Text.IO               as TIO
+import           Foreign.JNI                as J
 import           Language.Java              as J
 import           System.Environment         (getArgs,getEnv)
 --
@@ -37,6 +40,14 @@ main' = do
 
 main :: IO ()
 main = do
+  str <- newEmptyMVar
+  articles' <- getTimeTitleDescFromSrcWithHash "bloomberg"
+
+  void $ forkOS (runCoreNLP (take 1000 articles') str)
+  void $ forkOS (runCoreNLP (drop 1000 articles') str)
+  replicateM_ 2 (takeMVar str >>= putStrLn)
+
+runCoreNLP articles str = do
   clspath <- getEnv "CLASSPATH"
   J.withJVM [ B.pack ("-Djava.class.path=" ++ clspath) ] $ do
     pp <- prepare (def & (tokenizer .~ True)
@@ -47,7 +58,7 @@ main = do
                        . (constituency .~ True)
                        . (ner .~ True)
                   )
-    articles <- take 10 <$> getTimeTitleDescFromSrcWithHash "bloomberg"
     forM_ (catMaybes articles) $ \(hsh,_,_,x) -> do
       result <- runCoreNLPParser x pp
       BL.writeFile ("/home/modori/data/newsapianalyzed/" ++ (T.unpack hsh)) (A.encode result)
+    putMVar str ("Done!")
