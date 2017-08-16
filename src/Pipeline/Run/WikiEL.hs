2117e0dd255c3026e2e2ebcc245c61f621dc2c75
Doh Young Mo <modohyoung@gmail.com>
2017-08-16 06:24:15 +0000

    Modified WikiEL module.

--- src/Pipeline/Run/WikiEL.hs
+++ src/Pipeline/Run/WikiEL.hs
@@ -3,35 +3,18 @@
 
 module Pipeline.Run.WikiEL where
 
-import           Control.Lens
-import           Data.Text                          (Text)
-import           Data.Time.Calendar                 (fromGregorian)
-import           Language.Java                         as J
 --
-import qualified CoreNLP.Proto.CoreNLPProtos.Document  as D
-import           CoreNLP.Simple
 import           CoreNLP.Simple.Convert
-import           CoreNLP.Simple.Type
 import           CoreNLP.Simple.Type.Simplified
-import           Text.TaggedText
-import           WikiEL                           (loadEMtagger)
-import           WikiEL.Convert
-import           WikiEL.Type.FileFormat
-import           WikiEL.WikiEntityClass           (orgClass,personClass)
+import           WikiEL.EntityLinking
+import           WikiEL.WikiNamedEntityTagger
 
-getWikiEL :: Text -> J ('Class "edu.stanford.nlp.pipeline.AnnotationPipeline") -> IO [TagInfo Text]
-getWikiEL txt pp = do
-  emTagger <- loadEMtagger (EntityReprFile "/data/groups/uphere/wikidata/testset/uid")
-                           [(orgClass, ItemIDFile "/data/groups/uphere/wikidata/testset/ne.org"),
-                            (personClass, ItemIDFile "/data/groups/uphere/wikidata/testset/ne.person")]
-  let doc = Document txt (fromGregorian 2017 4 17)
-  ann <- annotate pp doc
-  rdoc <- protobufDoc ann
-  case rdoc of
-    Left  _ -> return []
-    Right d -> do
-      let sents = d ^.. D.sentence . traverse
-          f (NERSentence tokens) = tokens
-          neTokens =  concatMap (f.sentToNER) sents
-          linked_mentions = emTagger neTokens
-      return $ map (\x -> TagInfo { _taginfo_range = getRangeFromEntityMention x, _taginfo_metainfo = Just $ MetaInfo {_metainfo_info = getNEFromEntityMention x}, _taginfo_text = getNameFromEntityMention x}) linked_mentions
+getWikiEL sents emTagger = do
+  let mws = map (\(_,_,xs,_) -> xs) sents
+      mns = map (\(_,_,_,xs) -> xs) sents
+  let unNER (NERSentence tokens) = tokens
+      neTokens = concat $ map (\(x,y) -> (unNER $ sentToNER' x y)) (zip mws mns)
+      linked_mentions_all = emTagger neTokens
+      linked_mentions_resolved
+        = filter (\x -> let (_,_,pne) = _info x in case pne of Resolved _ -> True ; _ -> False) linked_mentions_all
+  return linked_mentions_resolved
