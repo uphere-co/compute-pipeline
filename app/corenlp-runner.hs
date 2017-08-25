7a2d745abc6ec8522585f37816672f27af2613e6
Doh Young Mo <modohyoung@gmail.com>
2017-08-25 02:29:42 +0000

    convert constraint to char based.

--- app/corenlp-runner.hs
+++ app/corenlp-runner.hs
@@ -11,7 +11,7 @@ import qualified Data.Aeson                 as A
 import qualified Data.ByteString.Char8      as B
 import qualified Data.ByteString.Lazy.Char8 as BL
 import           Data.Default
-import           Data.List                  (foldl',groupBy)
+import           Data.List                  (find,foldl',groupBy)
 import           Data.Maybe                 (catMaybes)
 import qualified Data.Text                  as T
 import qualified Data.Text.IO               as TIO
@@ -38,8 +38,8 @@ import           Pipeline.Source.NewsAPI.Article
 import           Pipeline.Util
 
 -- Load and Run
-main' :: IO ()
-main' = do
+main'' :: IO ()
+main'' = do
   (sensemap,sensestat,framedb,ontomap,emTagger,rolemap,subcats) <- loadConfig
   fps <- getFileListRecursively "/home/modori/data/newsapianalyzed"
   loaded' <- loadCoreNLPResult fps
@@ -52,20 +52,31 @@ contained (a,b) (c,d) = if (a >= c && b <=d) then True else False
 isContained (a,b) xs = foldl' (\acc x -> if ((a,b) `contained` x) then (acc || True) else (acc || False)) False xs
 
 -- Parse and Save
+main' :: IO ()
+main' = do
+  [src] <- getArgs
+  articles <- getTimeTitleDescFromSrcWithHash src
+  runCoreNLP articles
+
 main :: IO ()
-main = do
-  -- [src] <- getArgs
-  -- articles <- getTimeTitleDescFromSrcWithHash src
-  -- runCoreNLP articles
+main = preProcessing
+
+
+convertConstraintInCharIdx (b,e) tokss =
+  let tb' = find (\t -> (fst $ _token_tok_idx_range t) == b) (concat tokss)
+      te' = find (\t -> (snd $ _token_tok_idx_range t) == e) (concat tokss)
+  in case (tb',te') of
+    (Just tb, Just te) -> Just $ (fst $ _token_char_idx_range tb, snd $ _token_char_idx_range te)
+    otherwise          -> Nothing
+    
+preProcessing = do
   txt <- TIO.readFile "test.txt"
   (sensemap,sensestat,framedb,ontomap,emTagger,rolemap,subcats) <- loadConfig
   sents <- preRunCoreNLP txt
   let tokenss = catMaybes <$> sents ^.. traverse . sentenceToken
 
   let constraint = map (\x -> let irange = entityIRange x in (beg irange, end irange)) $ getWikiResolvedMentions emTagger sents
-  print constraint
-  print $ map (\ts -> groupBy (\x -> \y -> (T.take 2 x) == "NE" && (T.take 2 y) == "NE") $ map (\t -> if ((_token_tok_idx_range t) `isContained` constraint) then (T.append "NE" (_token_text t)) else (_token_text t)) ts) tokenss
-
+      constraint' = catMaybes $ map (\c -> convertConstraintInCharIdx c tokenss) constraint
   a <- flip evalStateT constraint $ do
     forM tokenss $ \tokens -> do
       forM tokens $ \t -> do
@@ -83,14 +94,17 @@ main = do
           else return (t,0)
 
   let f t1 t2 n1 n2 = case n1 of
-        0 -> (T.append (_token_text t1) (T.replicate ((fst $ _token_char_idx_range t2) - (snd $ _token_char_idx_range t1)) " "))
-        1 -> (T.append (_token_text t1) (T.replicate ((fst $ _token_char_idx_range t2) - (snd $ _token_char_idx_range t1)) "-"))
-        2 -> (T.append (_token_text t1) (T.replicate ((fst $ _token_char_idx_range t2) - (snd $ _token_char_idx_range t1)) " "))
+        0 -> fillGap t1 t2 " "
+        1 -> fillGap t1 t2 "-"
+        2 -> fillGap t1 t2 " "
+      gapLength t1 t2 = (fst $ _token_char_idx_range t2) - (snd $ _token_char_idx_range t1)
+      fillGap t1 t2 char = (T.append (_token_text t1) (T.replicate (gapLength t1 t2) char)) 
       result' = (map (\xs -> (map (\((t1,n1),(t2,n2)) -> f t1 t2 n1 n2) (zip xs (drop 1 xs))) ++ [(_token_text $ fst $ last xs)] ) a)
       result = T.intercalate "" $ concat result'
 
   TIO.putStrLn result
-
+  print constraint'
+  print tokenss
   
 preRunCoreNLP txt = do
   clspath <- getEnv "CLASSPATH"
