e2ee7365721beb4a0aeefd830190ced3f329900e
Doh Young Mo <modohyoung@gmail.com>
2017-08-28 04:24:47 +0000

    Styling work.

--- src/Pipeline/App/CoreNLPRunner.hs
+++ src/Pipeline/App/CoreNLPRunner.hs
@@ -6,38 +6,39 @@
 
 module Pipeline.App.CoreNLPRunner where
 
-import           Control.Exception          (SomeException,try)
+import           Control.Exception                            (SomeException,try)
 import           Control.Lens
-import           Control.Monad               (forM_,when)
+import           Control.Monad                                (forM_,when)
 import qualified Data.Aeson                            as A
 import qualified Data.ByteString.Char8                 as B
 import qualified Data.ByteString.Lazy.Char8            as BL
 import           Data.Default
 import           Data.Maybe
-import           Data.Text                                   (Text)
+import           Data.Text                                    (Text)
 import qualified Data.Text                             as T
+import qualified Data.Text.IO                          as TIO
 import           Language.Java                         as J
-import           System.Environment         (getArgs,getEnv)
-import           System.Directory           (doesFileExist)
-
+import           System.Directory                             (doesFileExist)
+import           System.Environment                           (getArgs,getEnv)
 --
-import           NewsAPI.DB                 (uploadAnalysis)
-import           NLP.Type.CoreNLP           (Sentence)
-
-import qualified NewsAPI.DB.Article         as Ar
-import           NLP.Type.PennTreebankII
 import qualified CoreNLP.Proto.CoreNLPProtos.Document  as D
-import qualified CoreNLP.Proto.HCoreNLPProto.ListTimex as T
 import qualified CoreNLP.Proto.CoreNLPProtos.Sentence  as S
+import qualified CoreNLP.Proto.HCoreNLPProto.ListTimex as T
 import           CoreNLP.Simple
 import           CoreNLP.Simple.Convert
 import           CoreNLP.Simple.Type
 import           CoreNLP.Simple.Util
-import 	       	 NLP.Type.CoreNLP
-import           Text.ProtocolBuffers.WireMessage            (messageGet)
---
-import           OntoNotes.App.Analyze.CoreNLP               (preRunParser,runParser)
+import           MWE
+import           NewsAPI.DB                                   (uploadAnalysis)
+import qualified NewsAPI.DB.Article                    as Ar
+import           NLP.Type.CoreNLP                             (Sentence)
+import           NLP.Type.PennTreebankII
+import           OntoNotes.App.Analyze
+import           OntoNotes.App.Analyze.CoreNLP                (preRunParser,runParser)
+import           OntoNotes.App.Analyze.SentenceStructure
 import           OntoNotes.App.Util
+import           Text.ProtocolBuffers.WireMessage             (messageGet)
+--
 import           Pipeline.Source.NewsAPI.Article
 import           Pipeline.Load
 import           Pipeline.Operation.DB
@@ -45,13 +46,8 @@ import           Pipeline.Run
 import           Pipeline.Util
 
 
-
-runCoreNLPParser = runParser
-
-preRunCoreNLPParser = preRunParser
-
-
-
+import           WikiEL.EntityLinking
+import           WikiEL.Misc
 
 
 preRunCoreNLP :: T.Text -> IO [Sentence]
@@ -64,12 +60,12 @@ preRunCoreNLP txt = do
                        . (lemma .~ True)
                        . (ner .~ True)
                   )
-    preRunCoreNLPParser pp txt
+    preRunParser pp txt
 
 runCoreNLP :: [Maybe (Ar.ArticleH,NewsAPIArticleContent)] -> IO ()
 runCoreNLP articles = do
   conn <- getConnection "dbname=mydb host=localhost port=65432 user=modori"
-  
+
   clspath <- getEnv "CLASSPATH"
   J.withJVM [ B.pack ("-Djava.class.path=" ++ clspath) ] $ do
     pp <- prepare (def & (tokenizer .~ True)
@@ -83,10 +79,39 @@ runCoreNLP articles = do
     forM_ (catMaybes articles) $ \(article,(hsh,_,_,x)) -> do
       fchk <- doesFileExist ("/home/modori/data/newsapianalyzed/" ++ (T.unpack hsh))
       when (not fchk) $ do
-        eresult <- try $ runCoreNLPParser pp x
+        eresult <- try $ runParser pp x
         case eresult of
           Left  (e :: SomeException) -> return ()
           Right result               -> do
             saveHashNameBSFileInPrefixSubDirs ("/home/modori/data/newsapianalyzed/" ++ (T.unpack hsh)) (BL.toStrict $ A.encode result)
             uploadAnalysis conn (mkNewsAPIAnalysisDB article)
-            
+
+
+
+-- Load and Run
+main'' :: IO ()
+main'' = do
+  (sensemap,sensestat,framedb,ontomap,emTagger,rolemap,subcats) <- loadConfig
+  fps <- getFileListRecursively "/home/modori/data/newsapianalyzed"
+  loaded' <- loadCoreNLPResult fps
+  putStrLn "Loading Completed."
+  let loaded = catMaybes $ map (\x -> (,) <$> Just (fst x) <*> snd x) loaded'
+  forM_ loaded $ \(fp,x) -> do
+    mapM_ TIO.putStrLn (sentStructure sensemap sensestat framedb ontomap emTagger rolemap subcats x)
+
+-- Parse and Save
+main' :: IO ()
+main' = do
+  [src] <- getArgs
+  articles <- getTimeTitleDescFromSrcWithHash src
+  runCoreNLP articles
+
+main :: IO ()
+main = do
+  txt <- TIO.readFile "test2.txt"
+  (sensemap,sensestat,framedb,ontomap,emTagger,rolemap,subcats) <- loadConfig
+  sents <- preRunCoreNLP txt
+  let wikiel = getWikiResolvedMentions emTagger sents
+      constraint = map (\x -> let irange = entityIRange x in (beg irange, end irange)) $ wikiel
+  preProcessing sents constraint
+
