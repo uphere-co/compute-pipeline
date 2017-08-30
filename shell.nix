5738f7803faab29c1cefdaf9a60d1b1efde49c9a
Doh Young Mo <modohyoung@gmail.com>
2017-08-30 06:29:04 +0000

    MeaningGraph from SRL is integrated.

--- shell.nix
+++ shell.nix
@@ -128,7 +128,7 @@ in
 
 stdenv.mkDerivation {
   name = "eventextractor-dev";
-  buildInputs = [ hsenv jdk ukb ];
+  buildInputs = [ hsenv jdk ukb graphviz ];
   shellHook = ''
     CLASSPATH="${corenlp_models}:${corenlp}/stanford-corenlp-3.7.0.jar:${corenlp}/protobuf.jar:${corenlp}/joda-time.jar:${corenlp}/jollyday.jar:${hsenv}/share/x86_64-linux-ghc-8.0.2/HCoreNLP-0.1.0.0/HCoreNLPProto.jar";
   '';
