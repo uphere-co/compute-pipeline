fc36b88718d6c83f22f6de37cb0b9eb30077452c
Doh Young Mo <modohyoung@gmail.com>
2017-09-06 01:49:40 +0000

    Limit the source.

--- nlp-query/src/Query/App/API.hs
+++ nlp-query/src/Query/App/API.hs
@@ -7,7 +7,7 @@ import           Control.Monad                     (forever)
 import qualified Data.Aeson         as A
 import           Data.Time.Clock                   (NominalDiffTime,UTCTime,addUTCTime,getCurrentTime)
 --
-import           NewsAPI.DB                        (getArticleByTime)
+import           NewsAPI.DB                        (getArticleBySourceAndTime)
 import qualified NewsAPI.DB.Article as Ar
 --
 
@@ -18,7 +18,7 @@ nominalDay = 86400
 oneDayArticles conn = do
   ctime <- getCurrentTime
   let yesterday = addUTCTime (-nominalDay) ctime 
-  articles <- getArticleByTime yesterday conn
+  articles <- getArticleBySourceAndTime conn "bloomberg" yesterday
   return articles
 
 getOneDayArticles conn = forever $ do
