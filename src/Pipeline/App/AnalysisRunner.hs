c2228da30938696b92a3930de5c74b22f0f59521
Doh Young Mo <modohyoung@gmail.com>
2017-09-11 05:54:07 +0000

    generateTextMG is separated out.

--- src/Pipeline/App/AnalysisRunner.hs
+++ src/Pipeline/App/AnalysisRunner.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-
 
 module Pipeline.App.AnalysisRunner where
 
@@ -11,7 +9,6 @@ import qualified Data.Aeson                 as A
 import qualified Data.ByteString.Base16     as B16
 import qualified Data.ByteString.Char8      as B
 import qualified Data.ByteString.Lazy.Char8 as BL8
-import           Data.Char                              (isSpace)
 import           Data.List                              (zip4)
 import           Data.Maybe
 import           Data.Text                              (Text)
@@ -38,6 +35,7 @@ import           WikiEL.EntityLinking                   (EntityMention)
 --
 import           Pipeline.Load
 import           Pipeline.Operation.DB                  (getConnection)
+import           Pipeline.Run
 import           Pipeline.Run.WikiEL
 import           Pipeline.Source.NewsAPI.Analysis
 import           Pipeline.Source.NewsAPI.Article
@@ -57,10 +55,6 @@ mkMGs conn apredata emTagger fp loaded = do
       wikilst = SRLWiki.mkWikiList dstr
       isNonFilter = True
 
-
-  atctitle <- fmap (T.unpack . (T.dropWhile isSpace)) $ getTitle ("/data/groups/uphere/repo/fetchfin/newsapi/Articles/bloomberg" </> filename)
-  
-  
   forM_ (zip4 [1..] sstrs mtokss mgs) $ \(i,sstr,mtks,mg') -> do
     -- fchk <- doesFileExist ("/home/modori/data/meaning_graph/" ++ filename ++ "_" ++ (show i) ++ ".png")
     -- when (not fchk) $ do
@@ -72,28 +66,9 @@ mkMGs conn apredata emTagger fp loaded = do
           when ((furthestPath graph >= 4 && numberOfIsland graph < 3) || isNonFilter) $ do
             let title = mkTextFromToken mtks  
                 mg = tagMG mg' wikilst
-            
-            let vertices = mg ^. mg_vertices
-                edges = mg ^. mg_edges
 
-            {-
-            putStrLn "======================================================================================="
+            generateTextMG mg filename (i,sstr,mtks,mg')
 
-            putStrLn ("filename : " ++ filename)
-            putStrLn ("title    : " ++ atctitle)
-            putStrLn ("descrip  : " ++ (T.unpack $ T.dropWhile isSpace $ mkTextFromToken mtks))
-            
-            forM_ vertices $ \v -> do
-              case v of
-                MGPredicate {..} -> putStrLn $ "MGPredicate :  " ++ (show $ v ^. mv_id) ++ "    " ++ (show (v ^. mv_range)) ++ "    " ++ (T.unpack (v ^. mv_frame)) ++ "    " ++ (T.unpack $ v ^. mv_verb . _1)
-                MGEntity    {..} -> putStrLn $ "MGEntity    :  " ++ (show $ v ^. mv_id) ++ "    " ++ (show (v ^. mv_range)) ++ "    " ++ (T.unpack (v ^. mv_text))
-
-            forM_ edges $ \e -> do
-              putStrLn $ "MGEdge       :  " ++ (T.unpack (e ^. me_relation)) ++ "    " ++  (show $ e ^. me_start) ++ "    "  ++ (show $ e ^. me_end)
-
-            putStrLn "=======================================================================================\n"
-            
-            -}          
             let dotstr = dotMeaningGraph (T.unpack $ mkLabelText title) mg
             putStrLn dotstr
             withCurrentDirectory "/home/modori/data/meaning_graph" $ do
--- src/Pipeline/Run.hs
+++ src/Pipeline/Run.hs
@@ -1,1 +1,35 @@
+{-# LANGUAGE RecordWildCards   #-}
+
 module Pipeline.Run where
+
+import           Control.Lens
+import           Control.Monad                          (forM_)
+import           Data.Char                              (isSpace)
+import qualified Data.Text as T
+import           System.FilePath                        ((</>))
+--
+import           MWE.Util                               (mkTextFromToken)
+import           SRL.Analyze.Type
+
+--
+import           Pipeline.Source.NewsAPI.Article        (getTitle)
+
+generateTextMG mg filename (i,sstr,mtks,mg') = do
+  atctitle <- fmap (T.unpack . (T.dropWhile isSpace)) $ getTitle ("/data/groups/uphere/repo/fetchfin/newsapi/Articles/bloomberg" </> filename)
+  let vertices = mg ^. mg_vertices
+      edges = mg ^. mg_edges  
+  putStrLn "======================================================================================="
+  putStrLn ("filename : " ++ filename)
+  putStrLn ("title    : " ++ atctitle)
+  putStrLn ("descrip  : " ++ (T.unpack $ T.dropWhile isSpace $ mkTextFromToken mtks))
+  forM_ vertices $ \v -> do
+    case v of
+      MGPredicate {..} -> putStrLn $ "MGPredicate :  " ++ (show $ v ^. mv_id) ++ "    " ++ (show (v ^. mv_range)) ++ "    " ++ (T.unpack (v ^. mv_frame)) ++ "    " ++ (T.unpack $ v ^. mv_verb . _1)
+      MGEntity    {..} -> putStrLn $ "MGEntity    :  " ++ (show $ v ^. mv_id) ++ "    " ++ (show (v ^. mv_range)) ++ "    " ++ (T.unpack (v ^. mv_text))
+  forM_ edges $ \e -> do
+    putStrLn $ "MGEdge       :  " ++ (T.unpack (e ^. me_relation)) ++ "    " ++  (show $ e ^. me_start) ++ "    "  ++ (show $ e ^. me_end)
+  putStrLn "=======================================================================================\n"
+
+
+          
+
