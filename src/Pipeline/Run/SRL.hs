6de219621a0f0bcae1cd05e743ec58622c9d858a
Doh Young Mo <modohyoung@gmail.com>
2017-09-13 05:03:51 +0000

    Label instead of index.

--- src/Pipeline/Run/SRL.hs
+++ src/Pipeline/Run/SRL.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE RecordWildCards  #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Pipeline.Run.SRL where
 
@@ -6,6 +7,7 @@ module Pipeline.Run.SRL where
 import           Control.Lens     ((^.),(^..),(^?),ix)
 import           Data.Graph
 import           Data.List        (find)
+import           Data.Maybe       (catMaybes,isNothing)
 import           Data.Text        (Text)
 import qualified Data.Text as T
 import qualified Data.Tree as Tr
@@ -26,13 +28,13 @@ cnvtVtxToMGV mg vtx =
       mv = find (\x -> (x ^. mv_id) == vtx) vertices
   in mv
   
-findLabel :: MGVertex -> Int -> Maybe Text
-findLabel mv i =
-  if ((mv ^. mv_id) == i)
-  then case mv of
-    v@(MGEntity {..})    -> Just (v ^. mv_text)
-    v@(MGPredicate {..}) -> Just (v ^. mv_frame)
-  else Nothing
+findLabel :: [MGVertex] -> Int -> Maybe Text
+findLabel mvs i =
+  let mr = find (\mv -> (mv ^. mv_id) == i) mvs
+  in case mr of
+    Nothing                     -> Nothing
+    Just (v@(MGEntity {..}))    -> Just (v ^. mv_text)
+    Just (v@(MGPredicate {..})) -> Just (v ^. mv_frame)
 
 findAgent :: MeaningGraph -> Graph -> Vertex -> Maybe Vertex
 findAgent mg grph vtx = case (cnvtVtxToMGV mg vtx) of
@@ -48,7 +50,6 @@ findAgentTheme mg grph vtx = case (cnvtVtxToMGV mg vtx) of
     False   -> Nothing
     True    -> (,) <$> Just vtx <*> (attached grph vtx ^? ix 0)
 
-      
 attached :: Graph -> Vertex -> [Vertex]
 attached grph vtx =
   let lnodes = concat $ fmap Tr.levels $ dfs grph [vtx]
@@ -64,7 +65,9 @@ mkARB mg = do
     Just graph -> do
       let mgpred = filter isMGPredicate (mg ^. mg_vertices)
           mgpredvtxs = (mgpred ^.. traverse . mv_id)
-          agents = map (\vtx -> findAgentTheme mg graph vtx) mgpredvtxs
+          agents = catMaybes $ map (\vtx -> findAgentTheme mg graph vtx) mgpredvtxs
+          vertices = mg ^. mg_vertices
+          agentsName = map (\(v1,v2) -> (,) <$> findLabel vertices v1 <*> findLabel vertices v2) agents
           reachList = map (\v -> reachable graph v) mgpredvtxs
-      print agents
+      print agentsName
 --      print $ map (\xs -> map (\x -> map (\g -> findLabel g x) (mg ^. mg_vertices)) xs) reachList
