5883158dc909314a293a76a8d11dea568573bd8d
Ian-Woo Kim <ianwookim@gmail.com>
2017-09-23 17:49:00 +0000

    refactor out filterARB

--- nlp-query/src/Query/App/API.hs
+++ nlp-query/src/Query/App/API.hs
@@ -46,7 +46,7 @@ import           Pipeline.Util                     (bstrHashToB16)
 --
 instance Hashable (PrepOr Text)
 
-instance Hashable ARB 
+instance Hashable ARB
 
 
 
@@ -81,7 +81,7 @@ loadExistingARB savepath = do
 oneDayArticles :: Connection -> Text -> IO [Ar.ArticleH]
 oneDayArticles conn txt = do
   ctime <- getCurrentTime
-  let yesterday = addUTCTime (-nominalDay) ctime 
+  let yesterday = addUTCTime (-nominalDay) ctime
   articles <- getArticleBySourceAndTime conn (T.unpack txt) yesterday
   return articles
 
@@ -104,9 +104,9 @@ nDayAnalyses conn txt n = do
 getNDayAnalyses :: Connection -> Text -> NominalDiffTime -> IO [(Text,Text,Maybe Bool,Maybe Bool,Maybe Bool)]
 getNDayAnalyses conn txt n = do
   analyses <- nDayAnalyses conn txt n
-  let anList = map (\x -> (T.pack $ bstrHashToB16 $ An._sha256 x, An._source x, An._corenlp x, An._srl x, An._ner x)) analyses 
+  let anList = map (\x -> (T.pack $ bstrHashToB16 $ An._sha256 x, An._source x, An._corenlp x, An._srl x, An._ner x)) analyses
   return anList
-  
+
 type API =    "recentarticle" :> Capture "ASource" T.Text :> Get '[JSON] [RecentArticle]
          :<|> "recentanalysis" :> Capture "AnSource" T.Text :> Get '[JSON] [RecentAnalysis]
          :<|> "recentarb" :> Get '[JSON] [(FilePath,(UTCTime,[ARB]))]
@@ -129,11 +129,14 @@ run conn cfg = do
   void $ forkIO $ updateARB arbstore arbs
   runSettings settings =<< (mkApp conn arbs)
 
+
 mkApp :: Connection -> TVar [(FilePath, (UTCTime, [ARB]))] -> IO Application
 mkApp conn arbs = return $ simpleCors (serve recentarticleAPI (server conn arbs))
 
+
 server :: Connection -> TVar [(FilePath, (UTCTime, [ARB]))] -> Server API
 server conn arbs = (getArticlesBySrc conn) :<|> (getAnalysesBySrc conn) :<|> (getARB arbs)
+
 
 getArticlesBySrc :: Connection -> T.Text -> Handler [RecentArticle]
 getArticlesBySrc conn txt = do
@@ -141,25 +144,33 @@ getArticlesBySrc conn txt = do
   let result = map (\(i,hsh,src) -> RecentArticle i hsh src) list
   return result
 
+
 getAnalysesBySrc :: Connection -> T.Text -> Handler [RecentAnalysis]
 getAnalysesBySrc conn txt = do
   list <- liftIO $ getNDayAnalyses conn txt 10
   let result = map (\(hsh,src,mb1,mb2,mb3) -> RecentAnalysis hsh src mb1 mb2 mb3) list
   return result
 
-getARB :: TVar [(FilePath, (UTCTime, [ARB]))] -> Handler [(FilePath,(UTCTime,[ARB]))]
-getARB arbs = do
-  liftIO $ putStrLn "getARB called"
-  arbs1 <- liftIO $ readTVarIO arbs
-  let n = 100
-  let arbs' = take (2*n) $ sortBy (flip compare `on` (\(_,(ct,_)) -> ct)) arbs1
-      templst = do (f,(t,arbs'')) <- arbs' -- result_unfiltered
+
+filterARB :: Int -> [(FilePath,(UTCTime,[ARB]))] -> [(FilePath,(UTCTime,[ARB]))]
+filterARB n arbs =
+  let -- we start with two times more sets considering filter-out items.
+      arbs1 = take (2*n) $ sortBy (flip compare `on` (\(_,(ct,_)) -> ct)) arbs
+      templst = do (f,(t,arbs'')) <- arbs1
                    arb <- arbs''
                    return (hash arb,f,t,arb)
       templst1 = (map (\(_h,t,f,x) -> ((t,f),x)) . map head . groupBy ((==) `on` (^._1)) . sortBy (compare `on` (^._1))) templst
       grouper lst = let ((t,f),_) = head lst
                         rs = map (^._2) lst
                     in (t,(f,rs))
-      templst2 = (map grouper . groupBy ((==) `on` (^._1)) .  sortBy (compare `on` (^._1))) templst1
-  return $ take n $ sortBy (flip compare `on` (\(_,(ct,_)) -> ct)) templst2
+      arbs2 = (map grouper . groupBy ((==) `on` (^._1)) .  sortBy (compare `on` (^._1))) templst1
+  in take n $ sortBy (flip compare `on` (\(_,(ct,_)) -> ct)) arbs2
 
+
+
+getARB :: TVar [(FilePath, (UTCTime, [ARB]))] -> Handler [(FilePath,(UTCTime,[ARB]))]
+getARB arbs = do
+  liftIO $ putStrLn "getARB called"
+  arbs1 <- liftIO $ readTVarIO arbs
+  let n = 100
+  return (filterARB n arbs1)
