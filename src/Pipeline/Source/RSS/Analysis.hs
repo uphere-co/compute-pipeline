429908bf320a1160ed794b38099d4a96fd6400ef
Doh Young Mo <modohyoung@gmail.com>
2017-09-25 07:44:52 +0000

    RSS file path getting routine is modified.

--- src/Pipeline/Source/RSS/Analysis.hs
+++ src/Pipeline/Source/RSS/Analysis.hs
@@ -3,26 +3,27 @@ module Pipeline.Source.RSS.Analysis where
 import           Control.Lens                      ((^.))
 import qualified Data.ByteString.Base16     as B16
 import qualified Data.ByteString.Lazy.Char8 as BL8  
+import           Data.Time.Clock                   (UTCTime)
 import           System.FilePath                   ((</>))
 --
 import           RSS.DB
-import qualified RSS.DB.Analysis            as An
+import qualified RSS.DB.Analysis            as RAn
 import           NLP.Shared.Type                   (PathConfig,dbstring)
 --
 import           Pipeline.Operation.DB
 import           Pipeline.Type
-  
-getAllRSSAnalysisFilePath :: PathConfig -> IO [FilePath]
+
+getAllRSSAnalysisFilePath :: PathConfig -> IO [(FilePath,UTCTime)]
 getAllRSSAnalysisFilePath cfg = do
   conn <- getConnection (cfg ^. dbstring)
   as <- getRSSAnalysisAll conn
-  return $ map (\x -> (take 2 x) </> x) $ getRSSAnalysisHashInB16 as
+  return $ map (\a -> (let hsh = anHshB16 a in (take 2 hsh) </> hsh,RAn._created a)) as
 
-getRSSAnalysisFilePathBySource :: PathConfig -> String -> IO [FilePath]
+getRSSAnalysisFilePathBySource :: PathConfig -> String -> IO [(FilePath,UTCTime)]
 getRSSAnalysisFilePathBySource cfg src = do
   conn <- getConnection (cfg ^. dbstring)
   as <- getRSSAnalysisBySource src conn
-  return $ map (\x -> (take 2 x) </> x) $ getRSSAnalysisHashInB16 as
+  return $ map (\a -> (let hsh = anHshB16 a in (take 2 hsh) </> hsh,RAn._created a)) as
 
-getRSSAnalysisHashInB16 :: [An.RSSAnalysisH] -> [FilePath]
-getRSSAnalysisHashInB16 as = map (BL8.unpack . BL8.fromStrict . B16.encode . An._sha256) as
+ranHshB16 :: RAn.RSSAnalysisH -> FilePath
+ranHshB16 a = (BL8.unpack . BL8.fromStrict . B16.encode . RAn._sha256) a
