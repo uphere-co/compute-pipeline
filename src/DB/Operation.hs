265c9c5a2c334fedd26d02a78cacb44548858915
Doh Young Mo <modohyoung@gmail.com>
2017-10-06 08:42:08 +0000

    Fixed the design.

--- src/DB/Operation.hs
+++ src/DB/Operation.hs
@@ -1,1 +1,32 @@
+{-# LANGUAGE Arrows #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module DB.Operation where
+
+import           Control.Arrow
+import           Control.Monad.IO.Class                     (liftIO)
+import           Data.ByteString.Char8                      (ByteString)
+import qualified Data.ByteString.Lazy.Char8     as BL
+import           Data.Maybe                                 (fromJust,isNothing)
+import           Data.Text                                  (Text)
+import qualified Data.Text                      as T
+import           Data.Time.Clock                            (UTCTime,getCurrentTime)
+import qualified Database.PostgreSQL.Simple     as PGS
+import           Opaleye                        hiding      (constant)
+import           Opaleye.Internal.Column                    (unColumn)
+--                                                                                                                            
+import           Model.Opaleye.ShowConstant                 (constant,safeCoerceToRep)
+import           Model.Opaleye.To
+import           DB.Schema.NewsAPI.Article                  (Article, ArticleP(Article), ArticleH)
+import qualified DB.Schema.NewsAPI.Article      as A
+--
+import           DB.Type
+
+uploadArticle :: (ToArticle a) => PGS.Connection -> a -> IO ()
+uploadArticle conn x = do
+  let a = toArticle x
+  runInsert conn A.table $
+    A.newArticle (_hash a) (_source a) (_created a)
+  return ()
--- src/DB/Type.hs
+++ src/DB/Type.hs
@@ -3,14 +3,12 @@
 
 module DB.Type where
 
-import Data.ByteString.Char8 (ByteString)
-import Data.Text             (Text)
-import Data.Time.Clock       (UTCTime)
+import           Data.ByteString.Char8             (ByteString)
+import           Data.Text                         (Text)
+import           Data.Time.Clock                   (UTCTime)
+import qualified Database.PostgreSQL.Simple as PGS
+--
 
-class UpHereDB a where
-  upload  :: a -> IO ()
-  upload2 :: a -> IO ()
-  update  :: a -> IO ()
 
 data ArticleDB = ArticleDB
   { _id      :: Int
@@ -19,11 +17,6 @@ data ArticleDB = ArticleDB
   , _created :: UTCTime
   } deriving (Show)
 
-instance (Article a) => UpHereDB a where
-  upload atl = do
-    let article = toArticle atl
-    return ()
-    
 {-
 data AnalysisDB = AnalysisDB
   {
@@ -31,7 +24,7 @@ data AnalysisDB = AnalysisDB
 data ArticleErrorDB = ArticleErrorDB { }
 -}
 
-class Article a where
+class ToArticle a where
   toArticle :: a -> ArticleDB
 
 
