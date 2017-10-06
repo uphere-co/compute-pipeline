59ec0dfa7768b778487db30e74fe54ce3affd1ad
Doh Young Mo <modohyoung@gmail.com>
2017-10-06 08:21:18 +0000

    class and type instances are defined for article.

--- src/DB/Type.hs
+++ src/DB/Type.hs
@@ -1,19 +1,44 @@
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module DB.Type where
+
+import Data.ByteString.Char8 (ByteString)
+import Data.Text             (Text)
+import Data.Time.Clock       (UTCTime)
 
 class UpHereDB a where
   upload  :: a -> IO ()
   upload2 :: a -> IO ()
   update  :: a -> IO ()
 
-data ArticleDB = ArticleDB { }
-data AnalysisDB = AnalysisDB { }
+data ArticleDB = ArticleDB
+  { _id      :: Int
+  , _hash    :: ByteString
+  , _source  :: Text
+  , _created :: UTCTime
+  } deriving (Show)
+
+instance (Article a) => UpHereDB a where
+  upload atl = do
+    let article = toArticle atl
+    return ()
+    
+{-
+data AnalysisDB = AnalysisDB
+  {
+  }
 data ArticleErrorDB = ArticleErrorDB { }
+-}
 
 class Article a where
   toArticle :: a -> ArticleDB
 
+
+{-
 class Analysis a where
   toAnalysis :: a -> AnalysisDB
 
 class ArticleError a where
   toArticleError :: a -> ArticleErrorDB
+-}
