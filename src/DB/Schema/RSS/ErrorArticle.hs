919e857449ec73ed2dbf34849305c0fe15cc4ae5
Doh Young Mo <modohyoung@gmail.com>
2017-10-19 04:52:24 +0000

    ErrorArticle module is added.

---
+++ src/DB/Schema/RSS/ErrorArticle.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
+
+module DB.Schema.RSS.ErrorArticle where
+
+import Data.ByteString.Char8
+import Data.Text
+import Data.Time.LocalTime
+import Data.Time.Clock
+import Opaleye                    hiding (constant)
+import Model.Opaleye.TH
+import Model.Opaleye.ShowConstant (constant)
+import Prelude
+
+$(makeTypes [d|
+    data ErrorArticle = ErrorArticle { _sha256 :: ByteString
+                                     , _source :: Text
+                                     , _created :: UTCTime
+                                     }
+                 deriving Show |])
+
+$(makeAdaptorAndInstance "pArticleError" ''ArticleErrorP)
+
+$(makeTable "articleerror" 'pArticleError ''ArticleErrorP)
+
+queryAll :: Query (To Column ArticleError)
+queryAll = queryTable table 
+
+-- smart constructor for inserting a new value.
+newArticleError :: ByteString
+                -> Text
+                -> UTCTime
+                -> To Maybe (To Column ArticleError)
+newArticleError s sn ct
+  = ArticleError (Just (constant s))
+                 (Just (constant sn))
+                 (Just (constant ct))
+
+-- The PostgreSQL table was created as follows.
+
+-- create table articleerror (
+--   sha256 bytea NOT NULL,
+--   source text NOT NULL,
+--   created timestamp with time zone,
+
+--   constraint unique_sha256_error UNIQUE (sha256)
+-- );
