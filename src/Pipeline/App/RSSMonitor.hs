2871de07f56ec4027839c86f2e95af09ec95fef4
Doh Young Mo <modohyoung@gmail.com>
2017-11-23 15:36:37 +0000

    Clean-up the code.

--- src/Pipeline/App/RSSMonitor.hs
+++ src/Pipeline/App/RSSMonitor.hs
@@ -2,11 +2,12 @@
 
 module Pipeline.App.RSSMonitor where
 
+import           Control.Applicative       (many)
 import           Control.Lens              ((^.))
 import           Control.Monad             (forM_)
 import           Control.Monad.IO.Class    (liftIO)
-import           Data.Attoparsec.Text
-import qualified Data.Attoparsec.Text as A
+import           Control.Monad.State.Lazy
+import           Control.Monad.Trans.Either (EitherT(..))
 import           Data.Text                 (Text)
 import qualified Data.Text            as T
 --
@@ -14,22 +15,8 @@ import           NER
 import           NLP.Shared.Type           (ItemRSS(..),description,link,pubDate,title)
 import           RSS.Load
 import           Text.Search.Generic.SearchTree
+import           Text.Search.ParserCustom
 import           Text.Search.SearchTree
---
-
--- companyParser :: Parser Text
-companyParser forest = do
-  word <- T.pack <$> many1 letter
-  skipSpace
-  let result =  searchFunc forest ["Apple"]
-  if result /= [[]]
-    then do
-    word <- T.pack <$> many1 letter
-    return word
-    else do
-    return word
-
-
 
 
 loadCompanies = do
@@ -42,26 +29,13 @@ loadForest companies = do
   return forest
 
 printAll cfg = do
-  nt <- loadNameTable 
-  companies <- getCompanyList nt
-
-  let forest = foldr addTreeItem [] (map T.words companies)
-  print $ searchFunc forest ["Apple"]
-  {-
+  forest <- loadForest =<< loadCompanies
   items <- loadAllRSSItems cfg
+  forM_ items $ \item -> do
+    print (item ^. description)
 
-
-  let f i = T.toLower (i ^. description)
-      citems = filter (\i -> any (\x -> T.toLower x `T.isInfixOf` (f i)) companies) items
-
-  forM_ citems $ \citem -> do
-    print (citem ^. description)
-  -}
-
-printAll2 forest = do
-  print $ searchFunc forest ["Apple"]
-
-parseTest forest = do
-  print $ parseOnly (companyParser forest) testSen1
-
-testSen1 = "Consumer Reports said on Wednesday its tests confirmed that putting Apple Inc's (AAPL.O) new iPhone into a case fixes the reception problems that some users have experienced."
+parseTest = do
+  forest <- loadForest =<< loadCompanies
+  let txts = T.words testSen1
+  let s = runState (runEitherT (many $ pTreeAdvG forest)) txts
+  print s
