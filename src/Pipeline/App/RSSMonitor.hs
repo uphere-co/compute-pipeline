8ef5ecb23050868acf91a6ce9d12f9bfef721e98
Doh Young Mo <modohyoung@gmail.com>
2017-12-11 11:26:16 +0000

    Use of event-analyzer.

--- src/Pipeline/App/RSSMonitor.hs
+++ src/Pipeline/App/RSSMonitor.hs
@@ -14,6 +14,7 @@ import qualified Data.ByteString.Char8      as B
 import           Data.Char                      (isSpace)
 import           Data.Default                   (def)
 import           Data.Either                    (isRight)
+import           Data.List                      (foldl')
 import           Data.Maybe                     (catMaybes)
 import           Data.Text                      (Text)
 import qualified Data.Text                  as T
@@ -22,11 +23,12 @@ import           System.Environment             (getEnv)
 --
 import           CoreNLP.Simple                 (prepare)
 import           CoreNLP.Simple.Type            (constituency,lemma,ner,postagger,sutime,tokenizer,words2sentences)
+import           EventAnalyzer.Classification   (identifyCommoditiesEvent)
 import           Lexicon.Data                   (loadLexDataConfig)
 import           NER
 import           NER.Load
 import           NER.Type
-import           NLP.Shared.Type                (ItemRSS(..),PathConfig,dbstring,description,lexconfigpath,link,pubDate,title)
+import           NLP.Shared.Type                (EventClass(..),ItemRSS(..),PathConfig,dbstring,description,lexconfigpath,link,pubDate,title)
 import           RSS.Load
 import           SRL.Analyze                    (loadConfig)
 import           SRL.Analyze.CoreNLP            (runParser)
@@ -38,57 +40,89 @@ import           Pipeline.Operation.DB          (closeConnection,getConnection)
 import           Pipeline.Run.CoreNLP           (tameDescription)
 
 
-
-
 tokenizeText = T.split (\c -> (isSpace c) || (c == '\8217'))
 
-
 printAll cfg pp = do
-  cfgG <- (\ec -> case ec of {Left err -> error err;Right cfg -> return cfg;}) =<< loadLexDataConfig (cfg ^. lexconfigpath)
-  (apredata,netagger,forest) <- loadConfig False cfgG
+--  cfgG <- (\ec -> case ec of {Left err -> error err;Right cfg -> return cfg;}) =<< loadLexDataConfig (cfg ^. lexconfigpath)
+--  (apredata,netagger,forest) <- loadConfig False cfgG
   items <- fmap (take 10000) $ loadAllRSSItems cfg
-  mfitems <- forM (zip [1..] items) $ \(i,item) -> do
-    print i
-    let txts = tokenizeText $ (tameDescription $ item ^. description)
-        s = runState (runEitherT (many $ pTreeAdvG forest)) txts
-    eprst <- try $ runParser pp (tameDescription $ item ^. description) 
+--  let totaln = length items
 
+  let ecs = map (\x -> identifyCommoditiesEvent (tameDescription $ x ^. description)) items
+
+  let stat = let n1 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "OilDemand&Supply")) x) then (acc+1) else acc) 0 ecs
+                 n2 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "Tanker")          ) x) then (acc+1) else acc) 0 ecs
+                 n3 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "StockMovement")   ) x) then (acc+1) else acc) 0 ecs
+                 n4 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "OilTrade")        ) x) then (acc+1) else acc) 0 ecs
+                 n5 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "ShaleOil")        ) x) then (acc+1) else acc) 0 ecs
+                 n6 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "MarketData")      ) x) then (acc+1) else acc) 0 ecs
+                 n7 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "OPEC")            ) x) then (acc+1) else acc) 0 ecs
+                 n8 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Soybean" Nothing                 ) x) then (acc+1) else acc) 0 ecs
+                 n9 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Oats" Nothing                    ) x) then (acc+1) else acc) 0 ecs
+                 n10 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Rice" Nothing                   ) x) then (acc+1) else acc) 0 ecs
+                 n11 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Wheat" Nothing                  ) x) then (acc+1) else acc) 0 ecs
+                 n12 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Palm Oil" Nothing               ) x) then (acc+1) else acc) 0 ecs
+                 n13 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Corn" Nothing                   ) x) then (acc+1) else acc) 0 ecs
+             in (n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13)
+
+  print stat
+      {-
+                    
+  forM_ (zip [1..] items) $ \(i,item) -> do
+    let txt = tameDescription $ item ^. description
+
+        txts = tokenizeText txt
+        s = runState (runEitherT (many $ pTreeAdvG forest)) txts
+
+--    eprst <- try $ runParser pp (tameDescription $ item ^. description) 
+    let ec = identifyCommoditiesEvent txt
+    when (((not . null) ec) && (T.length txt < 450)) $ do
+      print (ec,txt)
+
+-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    {-
     if (isRight (fst s))
       then do
       let Right s' = fst s
-      wlsts <- case eprst of
+
+      if (length s' > 0)
+        then do
+        if (["Royal","Dutch","Shell"] `elem` s')
+          then print ((show i) ++ "/" ++ (show totaln),s',txts)
+          else return ()
+--        print "Matched"
+--        print (s',txts)
+--        _ <- getChar
+        return (Just item)
+        else do
+--        print "Not Matched"
+--        print (txts)
+--        _ <- getChar
+        return Nothing
+      else return Nothing
+    -}
+
+--  print (length $ catMaybes mlist)
+{-      wlsts <- case eprst of
         Left (e :: SomeException) -> return []
         Right prst -> getWikiList apredata netagger forest prst
       if (((length s') + (length wlsts)) > 0)
         then return (Just "Matched")
-        else return Nothing
+        else return Nothing -}
 
-{-
-      if (length s' > 0)
-        then do
-        {-
-        print "Matched"
-        print (s',item ^. description)
-        wlsts <- case eprst of
-          Left (e :: SomeException) -> return []
-          Right prst -> getWikiList prst
-        _ <- getChar
-        return (Just item)
-        -}
-        else do
-        {-
-        print "Not Matched"
-        print (item ^. description)
-        wlsts <- case eprst of
-          Left (e :: SomeException) -> return []
-          Right prst -> getWikiList prst
-        _ <- getChar
-        return Nothing
-        -}
--}
-      else return Nothing
-  print $ length items
-  print $ length (catMaybes mfitems)
 
 
 runWithCoreNLP :: PathConfig -> IO ()
