a7a4b53e9f2a2256495d3636bc1b9da41bdd097f
Doh Young Mo <modohyoung@gmail.com>
2017-12-13 11:42:50 +0000

    Cleaned RSSMonitor app.

--- src/Pipeline/App/RSSMonitor.hs
+++ src/Pipeline/App/RSSMonitor.hs
@@ -43,32 +43,10 @@ import           Pipeline.Run.CoreNLP           (tameDescription)
 tokenizeText = T.split (\c -> (isSpace c) || (c == '\8217'))
 
 printAll cfg pp = do
---  cfgG <- (\ec -> case ec of {Left err -> error err;Right cfg -> return cfg;}) =<< loadLexDataConfig (cfg ^. lexconfigpath)
---  (apredata,netagger,forest) <- loadConfig False cfgG
+  cfgG <- (\ec -> case ec of {Left err -> error err;Right cfg -> return cfg;}) =<< loadLexDataConfig (cfg ^. lexconfigpath)
+  (apredata,netagger,forest,companyMap) <- loadConfig (False,False) cfgG
   items <- fmap (take 10000) $ loadAllRSSItems cfg
---  let totaln = length items
-
---  let ecs = map (\x -> identifyCommoditiesEvent (tameDescription $ x ^. description)) items
-
-{-  let stat = let n1 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "OilDemand&Supply")) x) then (acc+1) else acc) 0 ecs
-                 n2 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "Tanker")          ) x) then (acc+1) else acc) 0 ecs
-                 n3 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "StockMovement")   ) x) then (acc+1) else acc) 0 ecs
-                 n4 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "OilTrade")        ) x) then (acc+1) else acc) 0 ecs
-                 n5 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "ShaleOil")        ) x) then (acc+1) else acc) 0 ecs
-                 n6 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "MarketData")      ) x) then (acc+1) else acc) 0 ecs
-                 n7 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Energy" (Just "OPEC")            ) x) then (acc+1) else acc) 0 ecs
-                 n8 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Soybean" Nothing                 ) x) then (acc+1) else acc) 0 ecs
-                 n9 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Oats" Nothing                    ) x) then (acc+1) else acc) 0 ecs
-                 n10 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Rice" Nothing                   ) x) then (acc+1) else acc) 0 ecs
-                 n11 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Wheat" Nothing                  ) x) then (acc+1) else acc) 0 ecs
-                 n12 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Palm Oil" Nothing               ) x) then (acc+1) else acc) 0 ecs
-                 n13 = foldl' (\acc x -> if (any (== EventClass "Commodities" "Corn" Nothing                   ) x) then (acc+1) else acc) 0 ecs
-             in (n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13)
-
-  print stat -}
-
-  return ()
-      {-
+  let totaln = length items
                     
   forM_ (zip [1..] items) $ \(i,item) -> do
     let txt = tameDescription $ item ^. description
@@ -76,54 +54,24 @@ printAll cfg pp = do
         txts = tokenizeText txt
         s = runState (runEitherT (many $ pTreeAdvG forest)) txts
 
---    eprst <- try $ runParser pp (tameDescription $ item ^. description) 
-    let ec = identifyCommoditiesEvent txt
-    when (((not . null) ec) && (T.length txt < 450)) $ do
-      print (ec,txt)
-
--}
-
-
-
-
-
-
-
-
-
-
-
-
-
-    {-
     if (isRight (fst s))
       then do
       let Right s' = fst s
       if (length s' > 0)
         then do
-        if (["Royal","Dutch","Shell"] `elem` s')
+        if (((0 :: Int),["Royal","Dutch","Shell"]) `elem` s') -- This 0 will break search. Just for build.
           then print ((show i) ++ "/" ++ (show totaln),s',txts)
           else return ()
---        print "Matched"
---        print (s',txts)
---        _ <- getChar
+        print "Matched"
+        print (s',txts)
+        _ <- getChar
         return (Just item)
         else do
---        print "Not Matched"
---        print (txts)
---        _ <- getChar
+        print "Not Matched"
+        print (txts)
+        _ <- getChar
         return Nothing
       else return Nothing
-    -}
-
---  print (length $ catMaybes mlist)
-{-      wlsts <- case eprst of
-        Left (e :: SomeException) -> return []
-        Right prst -> getWikiList apredata netagger forest prst
-      if (((length s') + (length wlsts)) > 0)
-        then return (Just "Matched")
-        else return Nothing -}
-
 
 
 runWithCoreNLP :: PathConfig -> IO ()
