55385f57ac0da9e9444b370608379008b5097d62
Ian-Woo Kim <ianwookim@gmail.com>
2018-01-13 20:51:06 +0000

    reform shell.nix after D2157

--- shell.nix
+++ shell.nix
@@ -1,31 +1,5 @@
 { pkgs                  ? import <nixpkgs> {}
-, event-analyzer        ? <event-analyzer>
 , uphere-nix-overlay    ? <uphere-nix-overlay>
-, fetchfin              ? <fetchfin>
-, graph-algorithms      ? <graph-algorithms>
-, HCoreNLP              ? <HCoreNLP>
-, HFrameNet             ? <HFrameNet>
-, HWordNet              ? <HWordNet>
-, HUKB                  ? <HUKB>
-, lexicon               ? <lexicon>
-, lexicon-builder       ? <lexicon-builder>
-, multi-word-tagger     ? <multi-word-tagger>
-#, nlp-pipeline          ? <nlp-pipeline>
-#, nlp-shared-types      ? <nlp-shared-types>
-, nlp-types             ? <nlp-types>
-, OntoNotes             ? <OntoNotes>
-, predicate-matrix      ? <predicate-matrix>
-, PropBank              ? <PropBank>
-, semantic-role-labeler ? <semantic-role-labeler>
-, semantic-types        ? <semantic-types>
-, syntactic-analysis    ? <syntactic-analysis>
-, textview              ? <textview>
-, time-tagger           ? <time-tagger>
-#, uphere-db             ? <uphere-db>
-, uphere-network-util   ? <uphere-network-util>
-#, uphere-opaleye        ? <uphere-opaleye>
-, VerbNet               ? <VerbNet>
-, wiki-ner              ? <wiki-ner>
 }:
 
 let newpkgs = import pkgs.path {
@@ -45,58 +19,11 @@ let
   };
   corenlp = res_corenlp.corenlp;
   corenlp_models = res_corenlp.corenlp_models;
-  config1 = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.0.x.nix") { pkgs = newpkgs; };
-  haskellPackages1 = haskellPackages.override { overrides = config1; };
-  fastTextNix = import (semantic-role-labeler + "/fasttext/default.nix") {
-    inherit stdenv;
-    haskellPackages = haskellPackages1;
-  };
-  config2 =
-    self: super: {
-      "event-analyzer" = self.callPackage (import event-analyzer) {};
-      "fastText" = self.callPackage fastTextNix { inherit fasttext; };
-      "graph-algorithms" = self.callPackage (import graph-algorithms) {};
-      "HCoreNLP" = self.callPackage (import HCoreNLP) { inherit jdk corenlp corenlp_models; };
-      "HCoreNLP-Proto" = self.callPackage (import (HCoreNLP + "/HCoreNLP-Proto")) {};
-      "HFrameNet" = self.callPackage (import HFrameNet) {};      
-      "HWordNet" = self.callPackage (import HWordNet) {};
-      "lexicon" = self.callPackage (import lexicon) {};
-      "lexicon-builder" = self.callPackage (import lexicon-builder) {};      
-      "multi-word-tagger" = self.callPackage (import multi-word-tagger) {};
-      "nlp-types" = self.callPackage (import nlp-types) {};
-      "OntoNotes" = self.callPackage (import OntoNotes) {};
-      "predicate-matrix" = self.callPackage (import predicate-matrix) {};
-      "PropBank" = self.callPackage (import PropBank) {};
-      "rss-scraper" = self.callPackage (import (fetchfin + "/rss-scraper")) {};
-      "semantic-role-labeler" = self.callPackage (import semantic-role-labeler) {};
-      "semantic-types" = self.callPackage (import semantic-types) {};
-      "syntactic-analysis" = self.callPackage (import syntactic-analysis) {};
-      "textview" = self.callPackage (import textview) {};
-      "time-tagger" = self.callPackage (import time-tagger) {};      
-      "uphere-network-util" = self.callPackage (import uphere-network-util) {};      
-      "uphere-opaleye" = self.callPackage (import uphere-opaleye) {};
-      "VerbNet" = self.callPackage (import VerbNet) {};
-      "wiki-ner" = self.callPackage (import wiki-ner) {};
-      
-      #"nlp-shared-types" = self.callPackage (import nlp-shared-types) {};
-      #"uphere-db" = self.callPackage (import uphere-db) {};
-      #"newsapi" = self.callPackage (import (fetchfin + "/newsapi")) {};      
-      #"nlp-pipeline" = self.callPackage (import nlp-pipeline) {};
 
-
-      };
-
-  ukb = import (uphere-nix-overlay + "/nix/cpp-modules/ukb.nix") { inherit stdenv fetchgit fetchurl boost; };
-
-  config3 = import (HUKB + "/HUKB-driver/config.nix") { pkgs = newpkgs; inherit uphere-nix-overlay ukb; };
-  config4 =
-    self: super: {
-      "HUKB-driver" = self.callPackage (import (HUKB + "/HUKB-driver")) {};
-    };
-
-  newHaskellpkgs = haskellPackages.override {
-    overrides = self: super: config1 self super // config2 self super // config3 self super // config4 self super;
-  }; 
+  hsconfig = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-semantic-parser-api.nix")
+               { inherit corenlp corenlp_models fasttext fetchgit fetchurl haskellPackages jdk stdenv;
+                 haskellLib = haskell.lib; };
+  newHaskellpkgs = haskellPackages.override { overrides = hsconfig; };
 
   hsenv = newHaskellpkgs.ghcWithPackages (p: with p; [
             inline-java
@@ -131,12 +58,10 @@ let
             p.HUKB-driver
             p.nlp-types
             p.OntoNotes
-            p.predicate-matrix
             p.PropBank
             p.semantic-role-labeler
             p.wiki-ner
             p.textview
-            p.time-tagger
             p.uphere-network-util
           ]);
 
@@ -149,4 +74,3 @@ stdenv.mkDerivation {
     CLASSPATH="${corenlp_models}:${corenlp}/stanford-corenlp-3.7.0.jar:${corenlp}/protobuf.jar:${corenlp}/joda-time.jar:${corenlp}/jollyday.jar:${hsenv}/share/x86_64-linux-ghc-8.0.2/HCoreNLP-0.1.0.0/HCoreNLPProto.jar";
   '';
 }
-
