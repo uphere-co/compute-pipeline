7d3283e5bd68b5e16d81f60238d3d543f2f53915
Ian-Woo Kim <ianwookim@gmail.com>
2018-04-22 03:38:52 +0000

    use more generic query combinator following the changes in uphere-db

--- src/Pipeline/Source/RSS/Article.hs
+++ src/Pipeline/Source/RSS/Article.hs
@@ -5,15 +5,16 @@ module Pipeline.Source.RSS.Article where
 import           Control.Lens                      ((^.),to)
 import           Data.Aeson                        (eitherDecodeStrict)
 import qualified Data.ByteString.Base16     as B16
-import qualified Data.ByteString.Char8      as B   
-import qualified Data.ByteString.Lazy.Char8 as L8  
+import qualified Data.ByteString.Char8      as B
+import qualified Data.ByteString.Lazy.Char8 as L8
 import           Data.Maybe                        (fromJust,isJust,isNothing)
 import           Data.Text                         (Text)
 import qualified Data.Text                  as T
 import qualified Data.Text.Encoding         as TE
 import           Data.Time.Clock                   (UTCTime)
+import           Database.Beam                     (select,runSelectReturningList,(&&.))
 import           Database.Beam.Postgres            (runBeamPostgresDebug)
-import qualified Database.PostgreSQL.Simple as PGS 
+import qualified Database.PostgreSQL.Simple as PGS
 -- import           Opaleye                           (runQuery)
 import           System.Directory                  (doesFileExist)
 import           System.FilePath                   ((</>))
@@ -36,7 +37,12 @@ itempath = "RSSItem"
 getHashByTime :: PathConfig -> UTCTime -> IO [(Text,Text)]
 getHashByTime cfg time = do
   conn <- getConnection (cfg ^. dbstring)
-  articles <- getRSSArticleByTime conn time
+  articles <- runBeamPostgresDebug putStrLn conn $
+                runSelectReturningList $
+                  select $
+                    queryArticle (createdAfter time)
+
+  --   getRSSArticleByTime conn time
   PGS.close conn
   let mkPair :: RSSArticle -> (Text,Text)
       mkPair x = (x^.rssArticleSource, x^.rssArticleHash.to B16.encode.to TE.decodeUtf8)
@@ -53,10 +59,30 @@ getRSSArticleBy :: PathConfig -> SourceConstraint -> IO [Maybe (RSSArticle,ItemR
 getRSSArticleBy cfg sc = do
   conn <- getConnection (cfg ^. dbstring)
   articles <- case (whatConst sc) of
-                "SrcAndBetTime" -> runBeamPostgresDebug putStrLn conn (queryRSSArticleBySourceAndBetTime (fromJust $ _source sc) (fromJust $ _bTime sc) (fromJust $ _eTime sc))
-                "BetweenTime"   -> runBeamPostgresDebug putStrLn conn (queryRSSArticleBetweenTime (fromJust $ _bTime sc) (fromJust $ _eTime sc))
-                "Source"        -> getRSSArticleBySource conn (fromJust $ _source sc)
-                otherwise       -> return []
+                "SrcAndBetTime" ->
+                  runBeamPostgresDebug putStrLn conn $
+                    runSelectReturningList $
+                      select $
+                        let src = fromJust $ _source sc
+                            btime = fromJust $ _bTime sc
+                            etime = fromJust $ _eTime sc
+                        in queryArticle (\a ->     bySource src a
+                                               &&. createdAfter btime a
+                                               &&. createdBefore etime a)
+                "BetweenTime"   ->
+                  runBeamPostgresDebug putStrLn conn $
+                    runSelectReturningList $
+                      select $
+                        let btime = fromJust $ _bTime sc
+                            etime = fromJust $ _eTime sc
+                        in queryArticle (\a ->     createdAfter btime a
+                                               &&. createdBefore etime a)
+                "Source"        ->
+                  runBeamPostgresDebug putStrLn conn $
+                    runSelectReturningList $
+                      select $
+                        queryArticle (bySource (fromJust (_source sc)))
+                otherwise       -> pure []
   result <- flip mapM articles $ \x -> do
     let hshtxt = x^.rssArticleHash.to B16.encode.to TE.decodeUtf8
         src = x^.rssArticleSource
