9fbe1593db8fabd1e8f7fb345bc25ad0c96932fa
Ian-Woo Kim <ianwookim@gmail.com>
2018-11-05 15:50:18 +0000

    start orchestrator

--- compute-worker/app/orchestrator/Main.hs
+++ compute-worker/app/orchestrator/Main.hs
@@ -1,23 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
-module SO.MyCode
-  ( myApp
-  ) where
+module Main where
 
 import           Blaze.ByteString.Builder ( fromByteString )
-import           Control.Concurrent.MVar  ( MVar, modifyMVar )
+import           Control.Concurrent.MVar  ( MVar, modifyMVar, newMVar )
 import qualified Data.ByteString.Char8 as B
-import           Data.Semigroup           ( (<>) )
 import           Network.HTTP.Types       ( status200 )
 import           Network.Wai              ( Application, responseBuilder )
-------
-import           Worker.Type (WorkerRole)
+import           Network.Wai.Handler.Warp ( run )
 
-myApp :: MVar Int -> WorkerRole -> Application
-myApp countRef role _ respond = do
+
+application :: MVar Int -> Application
+application countRef _ respond = do
   modifyMVar countRef $ \count -> do
     let count' = count + 1102
         msg =    fromByteString (B.pack (show count'))
-              <> fromByteString (B.pack (show role))
     responseReceived <-
       respond $
         responseBuilder
@@ -25,3 +21,9 @@ myApp countRef role _ respond = do
           [("Content-Type", "text/plain")]
           msg
     pure (count',responseReceived)
+
+main :: IO ()
+main = do
+  putStrLn "orchestrator starts"
+  ref <- newMVar (0 :: Int)
+  run 32929 $ application ref
