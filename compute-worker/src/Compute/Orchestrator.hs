a00299c07dc5338d5b012a42c0af178707b18468
Ian-Woo Kim <ianwookim@gmail.com>
2018-11-11 00:13:45 +0000

    split out Compute.Orchestrator module.

---
+++ compute-worker/src/Compute/Orchestrator.hs
@@ -0,0 +1,110 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
+-- Orchestrator and workers have websocket communictation.
+--
+-- Orchestrator has two APIs related to shared object update notification:
+--
+-- * `/update` post API and
+-- * `/streaming` websocket API.
+--
+-- Each compute worker clients are supposed to connect `/streaming` websocket API when
+-- they are initialized. They are initialized with the current state of shared object
+-- path information retrieved from orchestrator via `/so` API.
+--
+-- By `/update`, a client (potential deployment script) will post a new path of share
+-- object. Then, orchestrator broadcasts the update to all of its clients. Worker has
+-- an event loop to be awakened by the push notification (using TVar retry-when-different
+-- cycle), and then it reloads SO file.
+
+module Compute.Orchestrator where
+
+import           Control.Concurrent.STM   ( TVar, newTVarIO, readTVarIO, writeTVar
+                                          , atomically  )
+import           Control.Concurrent.STM.TChan ( TChan, newBroadcastTChanIO
+                                              , dupTChan, readTChan, writeTChan )
+import           Control.Monad            ( forever, when )
+import           Control.Monad.IO.Class   ( liftIO )
+import           Data.List                ( find )
+import           Data.Text                ( Text )
+import qualified Data.Text as T
+import           Network.Wai.Handler.Warp ( runSettings, defaultSettings, setBeforeMainLoop, setPort )
+import           Network.WebSockets       ( Connection, forkPingThread, sendBinaryData )
+import           Servant                  ( Handler, Server, (:<|>)((:<|>))
+                                          , err404, throwError
+                                          , serve
+                                          )
+import           System.IO                ( hPutStrLn, stderr )
+------
+import           Worker.Type              ( ComputeConfig(..), CellConfig(..) )
+------
+import           Compute.Type             ( OrcApi, SOInfo(..), orcApi )
+
+-- * app
+
+runApp :: (ComputeConfig,FilePath) -> IO ()
+runApp (cfg,sofile) = do
+  let port = 3123
+      settings =
+        setPort port $
+        setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port)) $
+        defaultSettings
+      soinfo = SOInfo sofile
+  ref <- newTVarIO soinfo
+  chan <- newBroadcastTChanIO
+  runSettings settings $ serve orcApi (server cfg ref chan)
+
+
+server :: ComputeConfig
+       -> TVar SOInfo
+       -> TChan SOInfo  -- ^ write-only broadcast channel
+       -> Server OrcApi
+server cfg ref chan =
+       wsStream chan
+  :<|> getCompute cfg
+  :<|> getCell cfg
+  :<|> getSO ref
+  :<|> postUpdate ref chan
+
+
+getCompute :: ComputeConfig -> Handler ComputeConfig
+getCompute cfg = pure cfg
+
+getCell :: ComputeConfig -> Text -> Handler CellConfig
+getCell cfg name =
+  let mc = find (\c -> cellName c == name) (computeCells cfg)
+  in case mc of
+       Nothing -> throwError err404
+       Just c  -> pure c
+
+
+getSO :: TVar SOInfo -> Handler Text
+getSO ref = liftIO $ do
+  SOInfo fp <- readTVarIO ref
+  pure (T.pack fp)
+
+
+postUpdate :: TVar SOInfo -> TChan SOInfo -> Text -> Handler ()
+postUpdate ref chan txt = liftIO $ do
+  let fp = T.unpack txt
+  SOInfo fp' <- readTVarIO ref
+  when (fp /= fp') $ do
+    print fp
+    atomically $ do
+      writeTVar ref (SOInfo fp)
+      writeTChan chan (SOInfo fp)
+
+
+wsStream :: TChan SOInfo -> Connection -> Handler ()
+wsStream chan c = liftIO $ do
+  chan' <- atomically (dupTChan chan)
+  forkPingThread c 10
+  forever $ do
+    soinfo <- atomically $ readTChan chan'
+    sendBinaryData c soinfo
+
+
