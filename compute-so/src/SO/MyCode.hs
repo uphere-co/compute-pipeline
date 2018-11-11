2e9ebb1cd19274224b29c330217e139d3dc77fd2
Ian-Woo Kim <ianwookim@gmail.com>
2018-11-11 23:34:40 +0000

    revive workerMain skeleton.

--- compute-so/src/SO/MyCode.hs
+++ compute-so/src/SO/MyCode.hs
@@ -1,23 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 module SO.MyCode
   ( myApp
+  , workerMain
   ) where
 
 import           Blaze.ByteString.Builder ( fromByteString )
 import           Control.Concurrent       ( threadDelay )
 import           Control.Concurrent.MVar  ( MVar, modifyMVar )
+import           Control.Distributed.Process.Lifted ( Process, liftIO )
+import           Control.Distributed.Process.Node ( newLocalNode,runProcess )
 import           Control.Exception        ( bracket )
+import           Control.Monad            ( void )
+import           Control.Monad.Trans.Except ( runExceptT )
+import           Control.Monad.Trans.Reader ( runReaderT )
 import qualified Data.ByteString.Char8 as B
 import           Data.Semigroup           ( (<>) )
+import           Data.Text                ( Text )
 import qualified Data.Text             as T
 import           Network.HTTP.Types       ( status200 )
 import           Network.Transport        ( closeTransport )
 import           Network.Wai              ( Application, responseBuilder )
 import           System.IO                ( hPutStrLn, stderr )
 ------
-import           CloudHaskell.Util        ( tryCreateTransport )
+import           CloudHaskell.Util        ( expectSafe, newLogLock
+                                          , tryCreateTransport
+                                          )
+import           CloudHaskell.Type        ( Pipeline )
 import           Network.Transport.UpHere ( DualHostPortPair(..) )
-
+import           Compute.Task             ( rtable )
 ------
 import           Worker.Type              ( WorkerRole(..)
                                           , CellConfig(..)
@@ -82,6 +92,13 @@ workerMain =
     )
 -}
 
+testProcess :: Pipeline ()
+testProcess = do
+  liftIO $ hPutStrLn stderr "testProcess"
+  remotemsg <- expectSafe :: Pipeline Text
+  liftIO $ hPutStrLn stderr (show remotemsg)
+
+
 workerMain :: (WorkerRole,CellConfig) -> IO ()
 workerMain (Master,cellcfg) = do
   let netcfg = cellAddress cellcfg
@@ -93,7 +110,25 @@ workerMain (Master,cellcfg) = do
     closeTransport
     (\transport -> do
        hPutStrLn stderr "transport is created"
-       threadDelay 10000000
+       node <- newLocalNode transport rtable
+       lock <- newLogLock 0
+       runProcess node $ void $ flip runReaderT lock $ runExceptT $ testProcess
     )
-workerMain (Slave _,cellcfg) = do
+workerMain (Slave mcellcfg,scellcfg) = do
+  let snetcfg = cellAddress scellcfg
+      mnetcfg = cellAddress mcellcfg
+      dhpp = DHPP
+               (T.unpack (hostg snetcfg), show (port snetcfg))
+               (T.unpack (hostg snetcfg), show (port snetcfg))
+  bracket
+    (tryCreateTransport dhpp)
+    closeTransport
+    (\transport -> do
+       hPutStrLn stderr "transport is created"
+       node <- newLocalNode transport rtable
+       lock <- newLogLock 0
+       runProcess node $ void $ flip runReaderT lock $ runExceptT $ do
+         liftIO $ hPutStrLn stderr "in process"
+
+    )
   hPutStrLn stderr "nothing to do"
