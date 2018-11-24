df653cfa05f5b7851ce594ddfa1e8563c6574d85
Ian-Woo Kim <ianwookim@gmail.com>
2018-11-24 07:12:47 +0000

    implement heartBeat test. not success yet.

--- compute-worker-test/test/Test/InitHandshakeSpec.hs
+++ compute-worker-test/test/Test/InitHandshakeSpec.hs
@@ -1,14 +1,35 @@
+{-# LANGUAGE TypeApplications #-}
 module Test.InitHandshakeSpec where
 
-import Control.Concurrent (threadDelay,newEmptyMVar,takeMVar,putMVar)
-import Control.Distributed.Process (spawnChannelLocal,sendChan,receiveChan)
-import Control.Distributed.Process.Node (initRemoteTable,newLocalNode,runProcess)
+import Control.Concurrent          ( MVar
+                                   , threadDelay
+                                   , newEmptyMVar
+                                   , takeMVar
+                                   , putMVar
+                                   )
+import Control.Distributed.Process.Lifted
+                                   ( ProcessId
+                                   , getSelfPid
+                                   , spawnChannelLocal
+                                   , spawnLocal
+                                   , sendChan
+                                   , receiveChan
+                                   )
+import Control.Distributed.Process.Node.Lifted (initRemoteTable,newLocalNode,runProcess)
 import Control.Exception (bracket)
+import Control.Monad (void)
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Except (runExceptT)
+import Control.Monad.Trans.Reader (runReaderT)
 import Network.Transport (closeTransport)
 import Network.Transport.TCP (createTransport,defaultTCPParameters)
 import qualified System.IO as IO
 import Test.Hspec
+------
+import CloudHaskell.Client (heartBeatHandshake)
+import CloudHaskell.Server (withHeartBeat)
+import CloudHaskell.Type (Pipeline)
+import CloudHaskell.Util (expectSafe,newLogLock)
 
 
 
@@ -18,6 +39,24 @@ withTransport action = do
   case etrnsprt of
     Left e ->  fail (show e)
     Right transport -> action transport >>= \r -> closeTransport transport >> pure r
+
+
+testHandshake (lock_server,lock_client) = do
+  server_ping <- getSelfPid
+  spawnLocal (client lock_client server_ping)
+  server lock_server
+
+server :: MVar () -> Pipeline ()
+server lock_server = do
+  client_ping <- expectSafe @ProcessId
+  withHeartBeat client_ping (\_ -> pure ()) $ \client_main ->
+    liftIO $ putMVar lock_server ()
+
+client lock_client server_ping =
+  heartBeatHandshake server_ping $
+    liftIO $ putMVar lock_client ()
+
+
 
 spec :: Spec
 spec = do
@@ -33,7 +72,6 @@ spec = do
             lock <- liftIO $ newEmptyMVar
             sp <- spawnChannelLocal $ \rp -> do
                     y <- receiveChan rp
-                    -- liftIO $ IO.hPrint IO.stderr y
                     liftIO $ putMVar yref y
                     liftIO $ putMVar lock ()
             sendChan sp x
@@ -41,3 +79,18 @@ spec = do
 
         y <- takeMVar yref
         x `shouldBe` y
+
+  describe "Heartbeat test" $
+    it "should ping/pong heartbeat" $
+      withTransport $ \transport -> do
+        node <- newLocalNode transport initRemoteTable
+        lock <- newLogLock 0
+        lock_server <- newEmptyMVar
+        lock_client <- newEmptyMVar
+        runProcess node $
+          void $
+            flip runReaderT lock $
+              runExceptT $
+                testHandshake (lock_server,lock_client)
+        pure ()
+      `shouldReturn` ()
