d455bb1454816d8b764023ffc31913c89829a54c
Ian-Woo Kim <ianwookim@gmail.com>
2018-11-24 21:58:32 +0000

    put API on compute-so-types (still temporary.)

--- compute-so/src/SO/Handler/Web.hs
+++ compute-so/src/SO/Handler/Web.hs
@@ -1,26 +1,17 @@
 {-# LANGUAGE DataKinds          #-}
 {-# LANGUAGE ExplicitNamespaces #-}
 {-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE TypeOperators      #-}
 module SO.Handler.Web
   ( webApp
   ) where
 
 import           Control.Monad.IO.Class   ( liftIO )
-import           Data.Proxy               ( Proxy(..) )
 import           Data.Text                ( Text )
 import           Network.Wai              ( Application )
 import           Servant                  ( Handler, Server, serve )
-import           Servant.API              ( (:>), JSON, Post, ReqBody )
 ------
 import           CloudHaskell.QueryQueue  ( type QQVar, singleQuery )
-
-type SOAPI = "test" :> ReqBody '[JSON] Text :> Post '[JSON] Text
-
-
-soAPI :: Proxy SOAPI
-soAPI = Proxy
-
+import           Worker.API               ( type SOAPI, soAPI )
 
 getTest :: QQVar Text Text -> Text -> Handler Text
 getTest qqvar txt =
